interuss/dss

commit 0fb606a69685dfc6bc3857c05d554f831174eb99

Update docker configs and Postman test collection for public portal (#60)

datanode/src/storage_api.py
@@ -73,7 +73,7 @@ import slippy_util
 # VERSION = '1.0.2.003'  # slippy utility updates to support point/path/polygon
 # VERSION = '1.0.2.004'  # slippy non-breaking api changes to support path/polygon
 # VERSION = '1.1.0.005'  # api changes to support multi-grid GET/PUT/DEL
-VERSION = '1.1.1.006'  # Added public portal support
+VERSION = 'PublicPortal1.1.1.006'  # Added public portal support
 
 TESTID = None
 

datanode/src/storage_interface.py
@@ -475,7 +475,7 @@ class USSMetadataManager(object):
     log.debug('Getting multiple grid metadata for %s...', str(grids))
     combined_meta = None
     syncs = []
-    for x, y in grids:
+    for x, y in set(grids):
       if slippy_util.validate_slippy(z, x, y):
         (content, metadata) = self._get_raw(z, x, y)
         if metadata:

datanode/docker/Dockerfile_reverseproxy
@@ -29,6 +29,8 @@
 # Optional environment variables:
 #   INTERUSS_API_PORT_HTTP: Port on which the InterUSS API will be served via HTTP (default 8120).
 #   INTERUSS_API_PORT_HTTPS: Port on which the InterUSS API will be served via HTTPS (default 8121).
+#   SSL_CERT_NAME: File name of SSL certificate (default: cert.pem)
+#   SSL_KEY_NAME: File name of SSL private key (default: key.pem)
 
 FROM nginx
 

datanode/docker/Dockerfile_storageapi
@@ -15,7 +15,7 @@
 # Definition for Docker image that runs the InterUSS Platform storage API server.
 
 # To build an image from this Dockerfile:
-# docker image build -f Dockerfile_storageapi -t interussplatform/storage_api ../src
+# docker image build -f Dockerfile_storageapi -t interussplatform/storage_api:publicportal ../src
 
 # To run this image:
 # docker run \

datanode/docker/docker-compose.yaml
@@ -51,7 +51,7 @@ services:
       - ZOO_SERVERS=${ZOO_SERVERS:-server.1=0.0.0.0:2888:3888}
 
   storage_api:
-    image: interussplatform/storage_api:tcl4
+    image: interussplatform/storage_api:publicportal
     expose:
       - 5000
     environment:
@@ -73,5 +73,7 @@ services:
       - INTERUSS_API_PORT_HTTPS=${INTERUSS_API_PORT_HTTPS:-8121}
       - STORAGE_API_SERVER=storage_api
       - STORAGE_API_PORT=5000
+      - SSL_CERT_NAME
+      - SSL_KEY_NAME
     depends_on:
       - storage_api

datanode/docker/docker-compose_localssl.yaml
@@ -15,8 +15,12 @@
 # Modifies base docker-compose data node configuration to use keys present on the host file system.
 
 # Additional required environment variables:
-#   SSL_CERT_PATH: Path to SSL certificate named cert.pem.
-#   SSL_KEY_PATH: Path to SSL private key named key.pem.
+#   SSL_CERT_PATH: Path to SSL certificate named cert.pem, or per SSL_CERT_NAME.
+#   SSL_KEY_PATH: Path to SSL private key named key.pem, or per SSL_KEY_NAME.
+
+# Additional optional environment variables:
+#   SSL_CERT_NAME: File name of SSL certificate (default: cert.pem)
+#   SSL_KEY_NAME: File name of SSL private key (default: key.pem)
 
 # To use this configuration:
 # docker-compose -f docker-compose.yaml -f docker-compose_localssl.yaml -p datanode up
@@ -29,3 +33,6 @@ services:
     volumes:
       - ${SSL_CERT_PATH:?Environment variable SSL_CERT_PATH must be set}:/etc/ssl/certs
       - ${SSL_KEY_PATH:?Environment variable SSL_KEY_PATH must be set}:/etc/ssl/private
+    environment:
+      - SSL_CERT_NAME=${SSL_CERT_NAME:-cert.pem}
+      - SSL_KEY_NAME=${SSL_KEY_NAME:-key.pem}

datanode/docker/docker-compose_storageapitest.yaml
@@ -39,7 +39,7 @@ services:
       - ZOO_SERVERS=server.1=0.0.0.0:2888:3888
 
   storage_api:
-    image: interussplatform/storage_api
+    image: interussplatform/storage_api:publicportal
     ports:
       - "${INTERUSS_API_PORT:-8121}:${INTERUSS_API_PORT:-8121}"
     environment:

datanode/docker/nginx_entrypoint.sh
@@ -34,8 +34,8 @@ echo "
 
       listen ${INTERUSS_API_PORT_HTTPS:-8121} ssl;
       # server_name         www.example.com;
-      ssl_certificate     /etc/ssl/certs/cert.pem;
-      ssl_certificate_key /etc/ssl/private/key.pem;
+      ssl_certificate     /etc/ssl/certs/${SSL_CERT_NAME:-cert.pem};
+      ssl_certificate_key /etc/ssl/private/${SSL_KEY_NAME:-key.pem};
       ssl_protocols       TLSv1 TLSv1.1 TLSv1.2;
       ssl_ciphers         HIGH:!aNULL:!MD5;
 

commit 2c557e9a4e769a4e0293d34e3cf974ab9f3238dd

Fix bug introduced in 0fb606a69685dfc6bc3857c05d554f831174eb99

datanode/src/storage_api.py
@@ -73,7 +73,8 @@ import slippy_util
 # VERSION = '1.0.2.003'  # slippy utility updates to support point/path/polygon
 # VERSION = '1.0.2.004'  # slippy non-breaking api changes to support path/polygon
 # VERSION = '1.1.0.005'  # api changes to support multi-grid GET/PUT/DEL
-VERSION = 'PublicPortal1.1.1.006'  # Added public portal support
+# VERSION = 'PublicPortal1.1.1.006'  # Added public portal support
+VERSION = 'PublicPortal1.1.1.007'  # Fixed multi-cell bug
 
 TESTID = None
 

datanode/src/storage_interface.py
@@ -475,7 +475,7 @@ class USSMetadataManager(object):
     log.debug('Getting multiple grid metadata for %s...', str(grids))
     combined_meta = None
     syncs = []
-    for x, y in set(grids):
+    for x, y in grids:
       if slippy_util.validate_slippy(z, x, y):
         (content, metadata) = self._get_raw(z, x, y)
         if metadata: