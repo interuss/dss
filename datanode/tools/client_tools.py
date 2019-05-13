3eca186cd1d556b62e2adb2b75333e28b5d015ce
Benjamin Pelletier <BenjaminPelletier@users.noreply.github.com>
2019-05-13 11:56:17 -0700

    Record operation details from diagnostic logging tool (#81)
    This change extends the TCL4 diagnostic `monitor` tool to capture operation details in addition to the grid summaries it previously captured. To do this, it creates a TokenManager to automatically retrieve and appropriately cache tokens with different scopes, and provides a differently-scoped access token to retrieve operations from USS's than to retrieve the grid state.
    
    This tool was tested successfully during a UPP shakedown.

--- datanode/tools/client_tools.py
+++ datanode/tools/client_tools.py
@@ -16,41 +16,30 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import collections
+import datetime
 import json
 import os
+import sys
 
 import requests
+import requests.exceptions
 
 try:
   from urllib.request import Request, urlopen, HTTPError  # Python 3
 except ImportError:
   from urllib2 import Request, urlopen, HTTPError  # Python 2
 
-DEFAULT_AUTH_URL = 'https://utmalpha.arc.nasa.gov/fimsAuthServer/oauth/token?grant_type=client_credentials&scope=utm.nasa.gov_write.conflictmanagement'
+DEFAULT_AUTH_URL = 'https://utmalpha.arc.nasa.gov/fimsAuthServer/oauth/token?grant_type=client_credentials'
 DEFAULT_HOST = 'https://node4.tcl4.interussplatform.com:8121/'
 DEFAULT_REQUEST_PATH = 'GridCellsOperator/10?coords=48.832,-101.832,47.954,-101.832,47.954,-100.501,48.832,-100.501,48.832,-101.832&coord_type=polygon'
+EXPIRATION_BUFFER = 5  # seconds
 
+# Access token scope for accessing the InterUSS Platform.
+INTERUSS_SCOPE = 'utm.nasa.gov_write.conflictmanagement'
 
-def get_token(auth_key, auth_url):
-  """Call the specified OAuth server to retrieve an access token.
-
-  Args:
-    auth_key: Base64-encoded username and password.
-    auth_url: URL the provides an access token.
-
-  Returns:
-    Access token for TCL4 InterUSS Platform data node.
-
-  Raises:
-    ValueError: When access_token was not returned properly.
-  """
-  r = requests.post(auth_url, headers={'Authorization': 'Basic ' + auth_key})
-  result = r.content
-  result_json = json.loads(result)
-  if 'access_token' in result_json:
-    return result_json['access_token']
-  else:
-    raise ValueError('Error getting token: ' + r.content)
+# Access token scope for access individual USSs' operations.
+USS_SCOPE = 'utm.nasa.gov_write.operation'
 
 
 def get_metadata(token, url):
@@ -72,7 +61,26 @@ def get_metadata(token, url):
     json.loads(r.content)
   except ValueError:
     raise ValueError('Error getting metadata: ' + r.content)
+  except HTTPError as e:
+    raise ValueError('HTTPError getting metadata: ' + str(e))
+  except requests.exceptions.ConnectionError as e:
+    raise ValueError('ConnectionError getting metadata: ' + str(e))
   return r.content
+
+
+def get_operation(token, uss_baseurl, gufi):
+  """Retrieve details of a single operation from a USS."""
+  url = os.path.join(uss_baseurl, 'operations', gufi)
+  try:
+    response = requests.get(url, headers={
+      'Cache-Control': 'no-cache', 'Authorization': 'Bearer ' + token})
+    return response.content, response.status_code
+  except HTTPError as e:
+    return 'HTTPError: ' + str(e), 500
+  except ValueError as e:
+    return 'ValueError: ' + str(e), 400
+  except requests.exceptions.ConnectionError as e:
+    return 'Client ConnectionError: ' + str(e), 600
 
 
 def add_auth_arguments(parser):
@@ -133,3 +141,73 @@ def make_node_url(options):
   """
   return (options.node + ('' if options.node.endswith('/') else '/') +
           options.request_path)
+
+
+
+CachedToken = collections.namedtuple('CachedToken', ('value', 'expiration'))
+
+
+class TokenManager(object):
+  """Transparently provides access tokens, from cache when possible."""
+
+  def __init__(self, auth_url, auth_key):
+    """Create a TokenManager.
+
+    Args:
+      auth_url: URL the provides an access token.
+      auth_key: Base64-encoded username and password.
+    """
+    if '&scope=' in auth_url:
+      print('USAGE ERROR: The auth URL should now be provided without a scope '
+            'specified in its GET parameters.')
+      sys.exit(1)
+
+    self._auth_url = auth_url
+    self._auth_key = auth_key
+    self._tokens = {}
+
+  def _retrieve_token(self, scope):
+    """Call the specified OAuth server to retrieve an access token.
+
+    Args:
+      scope: Access token scope to request.
+
+    Returns:
+      CachedToken with requested scope.
+
+    Raises:
+      ValueError: When access_token was not returned properly.
+    """
+    url = self._auth_url + '&scope=' + scope
+    r = requests.post(url, headers={'Authorization': 'Basic ' + self._auth_key})
+    result = r.content
+    result_json = json.loads(result)
+    if 'access_token' in result_json:
+      token = result_json['access_token']
+      expires_in = int(result_json.get('expires_in', 0))
+      expiration = (datetime.datetime.utcnow() +
+                    datetime.timedelta(seconds=expires_in - EXPIRATION_BUFFER))
+      return CachedToken(token, expiration)
+    else:
+      raise ValueError('Error getting token: ' + r.content)
+
+  def get_token(self, scope):
+    """Retrieve a current access token with the requested scope.
+
+    Args:
+      scope: Access token scope to request.
+
+    Returns:
+      Access token content.
+
+    Raises:
+      ValueError: When access_token was not returned properly.
+    """
+    if scope in self._tokens:
+      if self._tokens[scope].expiration > datetime.datetime.utcnow():
+        return self._tokens[scope].value
+
+    print('')
+    print('Getting access token for %s...' % scope)
+    self._tokens[scope] = self._retrieve_token(scope)
+    return self._tokens[scope].value
