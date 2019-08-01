cff33c2e7157d8fbcba21790676620d510db0267
Thomas Voß <thomas@airmap.com>
2019-08-01 22:50:41 +0200

    Implement DeleteSubscription (#9)

--- pkg/dss/cockroach/store.go
+++ pkg/dss/cockroach/store.go
@@ -3,9 +3,74 @@ package cockroach
 import (
 	"context"
 	"database/sql"
+	"database/sql/driver"
+	"fmt"
+	"time"
 
+	"github.com/golang/protobuf/ptypes"
 	_ "github.com/lib/pq" // Pull in the postgres database driver
+	dspb "github.com/steeling/InterUSS-Platform/pkg/dssproto"
+	"go.uber.org/multierr"
 )
+
+// nullTime models a timestamp that could be NULL in the database. The model and
+// implementation follows prior art as in sql.Null* types.
+//
+// Please note that this is rather driver-specific. The postgres sql driver
+// errors out when trying to Scan a time.Time from a nil value. Other drivers
+// might behave differently.
+type nullTime struct {
+	Time  time.Time
+	Valid bool // Valid indicates whether Time carries a non-NULL value.
+}
+
+func (nt *nullTime) Scan(value interface{}) error {
+	if value == nil {
+		nt.Time = time.Time{}
+		nt.Valid = false
+		return nil
+	}
+
+	t, ok := value.(time.Time)
+	if !ok {
+		return fmt.Errorf("failed to cast database value, expected time.Time, got %T", value)
+	}
+	nt.Time, nt.Valid = t, ok
+
+	return nil
+}
+
+func (nt nullTime) Value() (driver.Value, error) {
+	if !nt.Valid {
+		return nil, nil
+	}
+	return nt.Time, nil
+}
+
+type subscriptionsRow struct {
+	id                string
+	owner             string
+	url               string
+	typesFilter       sql.NullString
+	notificationIndex int32
+	lastUsedAt        nullTime
+	beginsAt          nullTime
+	expiresAt         nullTime
+	updatedAt         time.Time
+}
+
+func (sr *subscriptionsRow) scan(row *sql.Row) error {
+	return row.Scan(&sr.id,
+		&sr.owner,
+		&sr.url,
+		&sr.typesFilter,
+		&sr.notificationIndex,
+		&sr.lastUsedAt,
+		&sr.beginsAt,
+		&sr.expiresAt,
+		&sr.updatedAt,
+	)
+}
 
 // Store is an implementation of dss.Store using
 // Cockroach DB as its backend store.
@@ -18,6 +83,156 @@ func (s *Store) Close() error {
 	return s.DB.Close()
 }
 
+// insertSubscriptionUnchecked inserts subscription into the store and returns
+// the resulting subscription including its ID.
+//
+// Please note that this function is only meant to be used in tests/benchmarks
+// to bootstrap a store with values. In particular, the function is not
+// validating timestamps of last updates.
+func (s *Store) insertSubscriptionUnchecked(ctx context.Context, subscription *dspb.Subscription) (*dspb.Subscription, error) {
+	const (
+		subscriptionQuery = `
+		INSERT INTO
+			subscriptions
+		VALUES
+			($1, $2, $3, $4, $5, $6, $7, $8, transaction_timestamp())
+		RETURNING
+			*`
+	)
+
+	sr := subscriptionsRow{
+		id:                subscription.GetId(),
+		owner:             subscription.GetOwner(),
+		url:               subscription.Callbacks.GetIdentificationServiceAreaUrl(),
+		notificationIndex: subscription.GetNotificationIndex(),
+	}
+
+	if ts := subscription.GetBegins(); ts != nil {
+		begins, err := ptypes.Timestamp(ts)
+		if err != nil {
+			return nil, err
+		}
+		sr.beginsAt.Time = begins
+		sr.beginsAt.Valid = true
+	}
+
+	if ts := subscription.GetExpires(); ts != nil {
+		expires, err := ptypes.Timestamp(ts)
+		if err != nil {
+			return nil, err
+		}
+		sr.expiresAt.Time = expires
+		sr.expiresAt.Valid = true
+	}
+
+	tx, err := s.Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := sr.scan(tx.QueryRowContext(
+		ctx,
+		subscriptionQuery,
+		sr.id,
+		sr.owner,
+		sr.url,
+		sr.typesFilter,
+		sr.notificationIndex,
+		sr.lastUsedAt,
+		sr.beginsAt,
+		sr.expiresAt,
+	)); err != nil {
+		return nil, multierr.Combine(err, tx.Rollback())
+	}
+
+	result := &dspb.Subscription{
+		Id:    sr.id,
+		Owner: sr.owner,
+		Callbacks: &dspb.SubscriptionCallbacks{
+			IdentificationServiceAreaUrl: sr.url,
+		},
+		NotificationIndex: sr.notificationIndex,
+	}
+
+	if sr.beginsAt.Valid {
+		ts, err := ptypes.TimestampProto(sr.beginsAt.Time)
+		if err != nil {
+			return nil, multierr.Combine(err, tx.Rollback())
+		}
+		result.Begins = ts
+	}
+
+	if sr.expiresAt.Valid {
+		ts, err := ptypes.TimestampProto(sr.expiresAt.Time)
+		if err != nil {
+			return nil, multierr.Combine(err, tx.Rollback())
+		}
+		result.Expires = ts
+	}
+
+	if err := tx.Commit(); err != nil {
+		return nil, err
+	}
+
+	return result, err
+}
+
+// DeleteSubscription deletes the subscription identified by "id" and
+// returns the deleted subscription.
+func (s *Store) DeleteSubscription(ctx context.Context, id string) (*dspb.Subscription, error) {
+	const (
+		subscriptionQuery = `
+		DELETE FROM
+			subscriptions
+		WHERE
+			id = $1
+		RETURNING
+			*`
+	)
+
+	tx, err := s.Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	sr := subscriptionsRow{}
+
+	if err := sr.scan(tx.QueryRowContext(ctx, subscriptionQuery, id)); err != nil {
+		return nil, multierr.Combine(err, tx.Rollback())
+	}
+
+	result := &dspb.Subscription{
+		Id:    sr.id,
+		Owner: sr.owner,
+		Callbacks: &dspb.SubscriptionCallbacks{
+			IdentificationServiceAreaUrl: sr.url,
+		},
+		NotificationIndex: int32(sr.notificationIndex),
+	}
+
+	if sr.beginsAt.Valid {
+		ts, err := ptypes.TimestampProto(sr.beginsAt.Time)
+		if err != nil {
+			return nil, multierr.Combine(err, tx.Rollback())
+		}
+		result.Begins = ts
+	}
+
+	if sr.expiresAt.Valid {
+		ts, err := ptypes.TimestampProto(sr.expiresAt.Time)
+		if err != nil {
+			return nil, multierr.Combine(err, tx.Rollback())
+		}
+		result.Expires = ts
+	}
+
+	if err := tx.Commit(); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
 // Bootstrap bootstraps the underlying database with required tables.
 //
 // TODO: We should handle database migrations properly, but bootstrap both us
@@ -25,17 +240,18 @@ func (s *Store) Close() error {
 func (s *Store) Bootstrap(ctx context.Context) error {
 	const query = `
 	CREATE TABLE IF NOT EXISTS subscriptions (
-		id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+		id UUID PRIMARY KEY,
 		owner STRING NOT NULL,
 		url STRING NOT NULL,
 		types_filter STRING,
+		notification_index INT4 DEFAULT 0,
+		last_used_at TIMESTAMPTZ,
+		begins_at TIMESTAMPTZ,
 		expires_at TIMESTAMPTZ,
-		updated_at TIMESTAMPTZ NOT NULL
-	);
-	CREATE TABLE IF NOT EXISTS subscriptions_status (
-		subscription_id UUID NOT NULL REFERENCES subscriptions (id) ON DELETE CASCADE,
-		notification_index INT8 DEFAULT 0,
-		last_used_at TIMESTAMPTZ
+		updated_at TIMESTAMPTZ NOT NULL,
+		INDEX begins_at_idx (begins_at),
+		INDEX expires_at_idx (expires_at),
+		CHECK (begins_at IS NULL OR expires_at IS NULL OR begins_at < expires_at)
 	);
 	CREATE TABLE IF NOT EXISTS cells_subscriptions (
 		cell_id INT64 NOT NULL,
@@ -47,14 +263,15 @@ func (s *Store) Bootstrap(ctx context.Context) error {
 		INDEX subscription_id_idx (subscription_id)
 	);
 	CREATE TABLE IF NOT EXISTS identification_service_areas (
-		id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+		id UUID PRIMARY KEY,
 		owner STRING NOT NULL,
 		url STRING NOT NULL,
 		starts_at TIMESTAMPTZ NOT NULL,
 		ends_at TIMESTAMPTZ NOT NULL,
 		updated_at TIMESTAMPTZ NOT NULL,
 		INDEX starts_at_idx (starts_at),
-		INDEX ends_at_idx (ends_at)
+		INDEX ends_at_idx (ends_at),
+		CHECK (starts_at IS NULL OR ends_at IS NULL OR starts_at < ends_at)
 	);
 	CREATE TABLE IF NOT EXISTS cells_identification_service_areas (
 		cell_id INT64 NOT NULL,
@@ -74,7 +291,6 @@ func (s *Store) Bootstrap(ctx context.Context) error {
 // cleanUp drops all required tables from the store, useful for testing.
 func (s *Store) cleanUp(ctx context.Context) error {
 	const query = `
-	DROP TABLE IF EXISTS subscriptions_status;
 	DROP TABLE IF EXISTS cells_subscriptions;
 	DROP TABLE IF EXISTS subscriptions;
 	DROP TABLE IF EXISTS cells_identification_service_areas;
---
+++ pkg/dss/server_test.go
@@ -0,0 +1,62 @@
+package dss
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	uuid "github.com/satori/go.uuid"
+	dspb "github.com/steeling/InterUSS-Platform/pkg/dssproto"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+type mockStore struct {
+	mock.Mock
+}
+
+func (ms *mockStore) Close() error {
+	return ms.Called().Error(0)
+}
+
+func (ms *mockStore) DeleteSubscription(ctx context.Context, id string) (*dspb.Subscription, error) {
+	args := ms.Called(ctx, id)
+	return args.Get(0).(*dspb.Subscription), args.Error(1)
+}
+
+func TestDeleteSubscriptionCallsIntoMockStore(t *testing.T) {
+	for _, r := range []struct {
+		name         string
+		id           string
+		subscription *dspb.Subscription
+		err          error
+	}{
+		{
+			name:         "subscription-is-returned-if-returned-from-store",
+			id:           uuid.NewV4().String(),
+			subscription: &dspb.Subscription{},
+		},
+		{
+			name: "error-is-returned-if-returned-from-store",
+			id:   uuid.NewV4().String(),
+			err:  errors.New("failed to look up subscription for ID"),
+		},
+	} {
+		t.Run(r.name, func(t *testing.T) {
+			store := &mockStore{}
+			store.On("DeleteSubscription", mock.Anything, r.id).Return(
+				r.subscription, r.err,
+			)
+			s := &Server{
+				Store: store,
+			}
+
+			response, err := s.DeleteSubscription(context.Background(), &dspb.DeleteSubscriptionRequest{
+				Id: r.id,
+			})
+			require.Equal(t, r.err, err)
+			require.EqualValues(t, r.subscription, response.GetSubscription())
+			require.True(t, store.AssertExpectations(t))
+		})
+	}
+}
