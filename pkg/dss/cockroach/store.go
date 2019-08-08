a0a2ffef5f89e993a6878d7f25b6350b65190110
steeling <teeling.sean@gmail.com>
2019-08-08 16:28:03 -0700

    Feature/secure crdb (#31)
    * Add cockroach secure URI builder

--- pkg/dss/cockroach/store.go
+++ pkg/dss/cockroach/store.go
@@ -3,6 +3,8 @@ package cockroach
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"fmt"
 	// Pull in the postgres database driver
 )
 
@@ -14,6 +16,7 @@ type Store struct {
 
 // Dial returns a Store instance connected to a cockroach instance available at
 // "uri".
+// https://www.cockroachlabs.com/docs/stable/connection-parameters.html
 func Dial(uri string) (*Store, error) {
 	db, err := sql.Open("postgres", uri)
 	if err != nil {
@@ -23,6 +26,33 @@ func Dial(uri string) (*Store, error) {
 	return &Store{
 		DB: db,
 	}, nil
+}
+
+func BuildURI(params map[string]string) (string, error) {
+	h := params["host"]
+	if h == "" {
+		return "", errors.New("missing crdb hostname")
+	}
+	p := params["port"]
+	if p == "" {
+		return "", errors.New("missing crdb port")
+	}
+	u := params["user"]
+	if u == "" {
+		return "", errors.New("missing crdb user")
+	}
+	ssl := params["ssl_mode"]
+	if ssl == "" {
+		return "", errors.New("missing crdb ssl_mode")
+	}
+	if ssl == "disable" {
+		return fmt.Sprintf("postgresql://%s@%s:%s?sslmode=disable", u, h, p), nil
+	}
+	dir := params["ssl_dir"]
+	if dir == "" {
+		return "", errors.New("missing crdb ssl_dir")
+	}
+	return fmt.Sprintf("postgresql://%s@%s:%s?sslmode=%s&sslrootcert=%s/ca.crt&sslcert=%s/client.%s.crt&sslkey=%s/client.%s.crt", u, h, p, ssl, dir, dir, u, dir, u), nil
 }
 
 type queryable interface {
