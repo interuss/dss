e4a8c38d05a9a72ae69f9f37bd0dc087096d3f6b
Thomas Voß <thomas@airmap.com>
2019-08-09 20:30:08 +0200

    Intercept outgoing errors (#36)

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -1,9 +1,42 @@
 package errors
 
 import (
+	"context"
+
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
+
+var (
+	errInternal = status.Error(codes.Internal, "Internal Server Error")
+)
+
+// Interceptor returns a grpc.UnaryServerInterceptor that inspects outgoing
+// errors and logs (to "logger") and replaces errors that are not *status.Status
+// instances or status instances that indicate an internal/unknown error.
+func Interceptor(logger *zap.Logger) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		resp, err = handler(ctx, req)
+		status, ok := status.FromError(err)
+
+		switch {
+		case !ok:
+			logger.Error("encountered error during unary server call", zap.String("method", info.FullMethod), zap.Error(err))
+			err = errInternal
+		case status.Code() == codes.Internal, status.Code() == codes.Unknown:
+			logger.Error("encountered internal error during unary server call",
+				zap.String("method", info.FullMethod),
+				zap.Stringer("code", status.Code()),
+				zap.String("message", status.Message()),
+				zap.Any("details", status.Details()),
+				zap.Error(err))
+			err = errInternal
+		}
+		return
+	}
+}
 
 func AlreadyExists(msg string) error {
 	return status.Error(codes.AlreadyExists, msg)
@@ -27,7 +60,7 @@ func BadRequest(msg string) error {
 
 func Internal(msg string) error {
 	// Log and obfuscate any errors.
-	return status.Error(codes.Internal, "Internal Server Error")
+	return errInternal
 }
 
 func Exhausted(msg string) error {
