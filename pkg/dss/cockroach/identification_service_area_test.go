76c87579ff628ebb4827d004dbf394cb3456b31c
David Sansome <me@davidsansome.com>
2019-09-02 13:11:03 +1000

    Change start_time and end_time to time_start and time_end in error messages

--- pkg/dss/cockroach/identification_service_area_test.go
+++ pkg/dss/cockroach/identification_service_area_test.go
@@ -192,7 +192,7 @@ func TestInsertISA(t *testing.T) {
 	}{
 		{
 			name:    "missing-end-time",
-			wantErr: "rpc error: code = InvalidArgument desc = IdentificationServiceArea must have an end_time",
+			wantErr: "rpc error: code = InvalidArgument desc = IdentificationServiceArea must have an time_end",
 		},
 		{
 			name:          "start-time-defaults-to-now",
@@ -203,7 +203,7 @@ func TestInsertISA(t *testing.T) {
 			name:      "start-time-in-the-past",
 			startTime: fakeClock.Now().Add(-6 * time.Minute),
 			endTime:   fakeClock.Now().Add(time.Hour),
-			wantErr:   "rpc error: code = InvalidArgument desc = IdentificationServiceArea start_time must not be in the past",
+			wantErr:   "rpc error: code = InvalidArgument desc = IdentificationServiceArea time_start must not be in the past",
 		},
 		{
 			name:          "start-time-slighty-in-the-past",
@@ -215,7 +215,7 @@ func TestInsertISA(t *testing.T) {
 			name:      "end-time-before-start-time",
 			startTime: fakeClock.Now().Add(20 * time.Minute),
 			endTime:   fakeClock.Now().Add(10 * time.Minute),
-			wantErr:   "rpc error: code = InvalidArgument desc = IdentificationServiceArea end_time must be after start_time",
+			wantErr:   "rpc error: code = InvalidArgument desc = IdentificationServiceArea time_end must be after time_start",
 		},
 		{
 			name:                "updating-keeps-old-times",
@@ -229,7 +229,7 @@ func TestInsertISA(t *testing.T) {
 			updateFromStartTime: fakeClock.Now().Add(-6 * time.Hour),
 			updateFromEndTime:   fakeClock.Now().Add(6 * time.Hour),
 			startTime:           fakeClock.Now().Add(-3 * time.Hour),
-			wantErr:             "rpc error: code = InvalidArgument desc = IdentificationServiceArea start_time must not be in the past",
+			wantErr:             "rpc error: code = InvalidArgument desc = IdentificationServiceArea time_start must not be in the past",
 		},
 		{
 			name:                "changing-start-time-to-future",
