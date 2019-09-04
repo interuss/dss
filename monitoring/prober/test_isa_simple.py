75121fcbade2abbc870227a9fa1fbd34c0f215b8
David Sansome <me@davidsansome.com>
2019-09-04 16:54:47 +1000

    Add some integration tests

---
+++ monitoring/prober/test_isa_simple.py
@@ -0,0 +1,119 @@
+"""Basic ISA tests:
+  - create the ISA with a 60 minute expiry
+  - get by ID
+  - search with earliest_time and latest_time
+  - delete
+"""
+
+import datetime
+import re
+
+import common
+
+
+def test_isa_does_not_exist(session, isa1_uuid):
+  resp = session.get('/identification_service_areas/{}'.format(isa1_uuid))
+  assert resp.status_code == 404
+  assert resp.json()['message'] == 'resource not found: {}'.format(isa1_uuid)
+
+
+def test_create_isa(session, isa1_uuid):
+  time_start = datetime.datetime.utcnow()
+  time_end = time_start + datetime.timedelta(minutes=60)
+
+  resp = session.put('/identification_service_areas/{}'.format(isa1_uuid), json={
+    'extents': {
+      'spatial_volume': {
+        'footprint': {
+          'vertices': common.VERTICES,
+        },
+        'altitude_lo': 20,
+        'altitude_hi': 400,
+      },
+      'time_start': time_start.strftime(common.DATE_FORMAT),
+      'time_end': time_end.strftime(common.DATE_FORMAT),
+    },
+    'flights_url': 'https://example.com/dss',
+  })
+  assert resp.status_code == 200
+
+  data = resp.json()
+  assert data['service_area']['id'] == isa1_uuid
+  assert data['service_area']['flights_url'] == 'https://example.com/dss'
+  assert data['service_area']['time_start'] == time_start.strftime(common.DATE_FORMAT)
+  assert data['service_area']['time_end'] == time_end.strftime(common.DATE_FORMAT)
+  assert re.match(r'[a-z0-9]{10,}$', data['service_area']['version'])
+  assert 'subscribers' in data
+
+
+def test_get_isa_by_id(session, isa1_uuid):
+  resp = session.get('/identification_service_areas/{}'.format(isa1_uuid))
+  assert resp.status_code == 200
+
+  data = resp.json()
+  assert data['service_area']['id'] == isa1_uuid
+  assert data['service_area']['flights_url'] == 'https://example.com/dss'
+
+
+def test_get_isa_by_search(session, isa1_uuid):
+  resp = session.get('/identification_service_areas?area={}'.format(common.GEO_POLYGON_STRING))
+  assert resp.status_code == 200
+  assert isa1_uuid in [x['id'] for x in resp.json()['service_areas']]
+
+
+def test_get_isa_by_search_earliest_time_included(session, isa1_uuid):
+  earliest_time = datetime.datetime.utcnow() + datetime.timedelta(minutes=59)
+  resp = session.get('/identification_service_areas'
+      '?area={}&earliest_time={}'.format(
+          common.GEO_POLYGON_STRING, earliest_time.strftime(common.DATE_FORMAT)))
+  assert resp.status_code == 200
+  assert isa1_uuid in [x['id'] for x in resp.json()['service_areas']]
+
+
+def test_get_isa_by_search_earliest_time_excluded(session, isa1_uuid):
+  earliest_time = datetime.datetime.utcnow() + datetime.timedelta(minutes=61)
+  resp = session.get('/identification_service_areas'
+      '?area={}&earliest_time={}'.format(
+          common.GEO_POLYGON_STRING, earliest_time.strftime(common.DATE_FORMAT)))
+  assert resp.status_code == 200
+  assert isa1_uuid not in [x['id'] for x in resp.json()['service_areas']]
+
+
+def test_get_isa_by_search_latest_time_included(session, isa1_uuid):
+  latest_time = datetime.datetime.utcnow() + datetime.timedelta(minutes=1)
+  resp = session.get('/identification_service_areas'
+      '?area={}&latest_time={}'.format(
+          common.GEO_POLYGON_STRING, latest_time.strftime(common.DATE_FORMAT)))
+  assert resp.status_code == 200
+  assert isa1_uuid in [x['id'] for x in resp.json()['service_areas']]
+
+
+def test_get_isa_by_search_latest_time_excluded(session, isa1_uuid):
+  latest_time = datetime.datetime.utcnow() - datetime.timedelta(minutes=1)
+  resp = session.get('/identification_service_areas'
+      '?area={}&latest_time={}'.format(
+          common.GEO_POLYGON_STRING, latest_time.strftime(common.DATE_FORMAT)))
+  assert resp.status_code == 200
+  assert isa1_uuid not in [x['id'] for x in resp.json()['service_areas']]
+
+
+def test_delete_isa(session, isa1_uuid):
+  # GET the ISA first to find its version.
+  resp = session.get('/identification_service_areas/{}'.format(isa1_uuid))
+  assert resp.status_code == 200
+  version = resp.json()['service_area']['version']
+
+  # Then delete it.
+  resp = session.delete('/identification_service_areas/{}/{}'.format(isa1_uuid, version))
+  assert resp.status_code == 200
+
+
+def test_get_deleted_isa_by_id(session, isa1_uuid):
+  resp = session.get('/identification_service_areas/{}'.format(isa1_uuid))
+  assert resp.status_code == 404
+
+
+def test_get_deleted_isa_by_search(session, isa1_uuid):
+  resp = session.get('/identification_service_areas?area={}'.format(common.GEO_POLYGON_STRING))
+  assert resp.status_code == 200
+  assert isa1_uuid not in [x['id'] for x in resp.json()['service_areas']]
---
+++ monitoring/prober/test_subscription_simple.py
@@ -0,0 +1,91 @@
+"""Basic subscription tests:
+  - create the subscription with a 60 minute expiry
+  - get by ID
+  - get by search
+  - delete
+"""
+
+import datetime
+import re
+
+import common
+
+
+def test_sub_does_not_exist(session, sub1_uuid):
+  resp = session.get('/subscriptions/{}'.format(sub1_uuid))
+  assert resp.status_code == 404
+  assert resp.json()['message'] == 'resource not found: {}'.format(sub1_uuid)
+
+
+def test_create_sub(session, sub1_uuid):
+  time_start = datetime.datetime.utcnow()
+  time_end = time_start + datetime.timedelta(minutes=60)
+
+  resp = session.put('/subscriptions/{}'.format(sub1_uuid), json={
+    'extents': {
+      'spatial_volume': {
+        'footprint': {
+          'vertices': common.VERTICES,
+        },
+        'altitude_lo': 20,
+        'altitude_hi': 400,
+      },
+      'time_start': time_start.strftime(common.DATE_FORMAT),
+      'time_end': time_end.strftime(common.DATE_FORMAT),
+    },
+    'callbacks': {
+      'identification_service_area_url': 'https://example.com/foo'
+    },
+  })
+  assert resp.status_code == 200
+
+  data = resp.json()
+  assert data['subscription']['id'] == sub1_uuid
+  assert data['subscription']['notification_index'] == 0
+  assert data['subscription']['callbacks'] == {
+    'identification_service_area_url': 'https://example.com/foo'
+  }
+  assert data['subscription']['time_start'] == time_start.strftime(common.DATE_FORMAT)
+  assert data['subscription']['time_end'] == time_end.strftime(common.DATE_FORMAT)
+  assert re.match(r'[a-z0-9]{10,}$', data['subscription']['version'])
+  assert 'service_areas' in data
+
+
+def test_get_sub_by_id(session, sub1_uuid):
+  resp = session.get('/subscriptions/{}'.format(sub1_uuid))
+  assert resp.status_code == 200
+
+  data = resp.json()
+  assert data['subscription']['id'] == sub1_uuid
+  assert data['subscription']['notification_index'] == 0
+  assert data['subscription']['callbacks'] == {
+    'identification_service_area_url': 'https://example.com/foo'
+  }
+
+
+def test_get_sub_by_search(session, sub1_uuid):
+  resp = session.get('/subscriptions?area={}'.format(common.GEO_POLYGON_STRING))
+  assert resp.status_code == 200
+  assert sub1_uuid in [x['id'] for x in resp.json()['subscriptions']]
+
+
+def test_delete_sub(session, sub1_uuid):
+  # GET the sub first to find its version.
+  resp = session.get('/subscriptions/{}'.format(sub1_uuid))
+  assert resp.status_code == 200
+  version = resp.json()['subscription']['version']
+
+  # Then delete it.
+  resp = session.delete('/subscriptions/{}/{}'.format(sub1_uuid, version))
+  assert resp.status_code == 200
+
+
+def test_get_deleted_sub_by_id(session, sub1_uuid):
+  resp = session.get('/subscriptions/{}'.format(sub1_uuid))
+  assert resp.status_code == 404
+
+
+def test_get_deleted_sub_by_search(session, sub1_uuid):
+  resp = session.get('/subscriptions?area={}'.format(common.GEO_POLYGON_STRING))
+  assert resp.status_code == 200
+  assert sub1_uuid not in [x['id'] for x in resp.json()['subscriptions']]
