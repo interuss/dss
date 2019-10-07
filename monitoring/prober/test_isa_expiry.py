cb4ba07bb7be1522c4d0dcbfe2e0802c1031df7d
Charlie Pisuraj <pcharlie@pcharlie-macbookair.roam.corp.google.com>
2019-10-07 15:29:12 -0700

    Scripts for E2E testing

--- monitoring/prober/test_isa_expiry.py
+++ monitoring/prober/test_isa_expiry.py
@@ -11,7 +11,7 @@ def test_create(session, isa1_uuid):
   time_end = time_start + datetime.timedelta(seconds=5)
 
   resp = session.put(
-      '/identification_service_areas/{}'.format(isa1_uuid),
+      '/v1/dss/identification_service_areas/{}'.format(isa1_uuid),
       json={
           'extents': {
               'spatial_volume': {
@@ -31,7 +31,7 @@ def test_create(session, isa1_uuid):
 
 def test_valid_immediately(session, isa1_uuid):
   # The ISA is still valid immediately after we create it.
-  resp = session.get('/identification_service_areas/{}'.format(isa1_uuid))
+  resp = session.get('/v1/dss/identification_service_areas/{}'.format(isa1_uuid))
   assert resp.status_code == 200
 
 
@@ -42,14 +42,14 @@ def test_sleep_5_seconds():
 
 def test_not_returned_by_id(session, isa1_uuid):
   # And we can't get it by ID...
-  resp = session.get('/identification_service_areas/{}'.format(isa1_uuid))
+  resp = session.get('/v1/dss/identification_service_areas/{}'.format(isa1_uuid))
   assert resp.status_code == 404
   assert resp.json()['message'] == 'resource not found: {}'.format(isa1_uuid)
 
 
 def test_not_returned_by_search(session, isa1_uuid):
   # Or by search.
-  resp = session.get('/identification_service_areas?area={}'.format(
+  resp = session.get('/v1/dss/identification_service_areas?area={}'.format(
       common.GEO_POLYGON_STRING))
   assert resp.status_code == 200
   assert isa1_uuid not in [x['id'] for x in resp.json()['service_areas']]
--- monitoring/prober/test_isa_simple.py
+++ monitoring/prober/test_isa_simple.py
@@ -13,7 +13,7 @@ import common
 
 
 def test_isa_does_not_exist(session, isa1_uuid):
-  resp = session.get('/identification_service_areas/{}'.format(isa1_uuid))
+  resp = session.get('/v1/dss/identification_service_areas/{}'.format(isa1_uuid))
   assert resp.status_code == 404
   assert resp.json()['message'] == 'resource not found: {}'.format(isa1_uuid)
 
@@ -23,7 +23,7 @@ def test_create_isa(session, isa1_uuid):
   time_end = time_start + datetime.timedelta(minutes=60)
 
   resp = session.put(
-      '/identification_service_areas/{}'.format(isa1_uuid),
+      '/v1/dss/identification_service_areas/{}'.format(isa1_uuid),
       json={
           'extents': {
               'spatial_volume': {
@@ -52,7 +52,7 @@ def test_create_isa(session, isa1_uuid):
 
 
 def test_get_isa_by_id(session, isa1_uuid):
-  resp = session.get('/identification_service_areas/{}'.format(isa1_uuid))
+  resp = session.get('/v1/dss/identification_service_areas/{}'.format(isa1_uuid))
   assert resp.status_code == 200
 
   data = resp.json()
@@ -61,7 +61,7 @@ def test_get_isa_by_id(session, isa1_uuid):
 
 
 def test_get_isa_by_search(session, isa1_uuid):
-  resp = session.get('/identification_service_areas?area={}'.format(
+  resp = session.get('/v1/dss/identification_service_areas?area={}'.format(
       common.GEO_POLYGON_STRING))
   assert resp.status_code == 200
   assert isa1_uuid in [x['id'] for x in resp.json()['service_areas']]
@@ -69,7 +69,7 @@ def test_get_isa_by_search(session, isa1_uuid):
 
 def test_get_isa_by_search_earliest_time_included(session, isa1_uuid):
   earliest_time = datetime.datetime.utcnow() + datetime.timedelta(minutes=59)
-  resp = session.get('/identification_service_areas'
+  resp = session.get('/v1/dss/identification_service_areas'
                      '?area={}&earliest_time={}'.format(
                          common.GEO_POLYGON_STRING,
                          earliest_time.strftime(common.DATE_FORMAT)))
@@ -79,7 +79,7 @@ def test_get_isa_by_search_earliest_time_included(session, isa1_uuid):
 
 def test_get_isa_by_search_earliest_time_excluded(session, isa1_uuid):
   earliest_time = datetime.datetime.utcnow() + datetime.timedelta(minutes=61)
-  resp = session.get('/identification_service_areas'
+  resp = session.get('/v1/dss/identification_service_areas'
                      '?area={}&earliest_time={}'.format(
                          common.GEO_POLYGON_STRING,
                          earliest_time.strftime(common.DATE_FORMAT)))
@@ -89,7 +89,7 @@ def test_get_isa_by_search_earliest_time_excluded(session, isa1_uuid):
 
 def test_get_isa_by_search_latest_time_included(session, isa1_uuid):
   latest_time = datetime.datetime.utcnow() + datetime.timedelta(minutes=1)
-  resp = session.get('/identification_service_areas'
+  resp = session.get('/v1/dss/identification_service_areas'
                      '?area={}&latest_time={}'.format(
                          common.GEO_POLYGON_STRING,
                          latest_time.strftime(common.DATE_FORMAT)))
@@ -99,7 +99,7 @@ def test_get_isa_by_search_latest_time_included(session, isa1_uuid):
 
 def test_get_isa_by_search_latest_time_excluded(session, isa1_uuid):
   latest_time = datetime.datetime.utcnow() - datetime.timedelta(minutes=1)
-  resp = session.get('/identification_service_areas'
+  resp = session.get('/v1/dss/identification_service_areas'
                      '?area={}&latest_time={}'.format(
                          common.GEO_POLYGON_STRING,
                          latest_time.strftime(common.DATE_FORMAT)))
@@ -109,23 +109,23 @@ def test_get_isa_by_search_latest_time_excluded(session, isa1_uuid):
 
 def test_delete_isa(session, isa1_uuid):
   # GET the ISA first to find its version.
-  resp = session.get('/identification_service_areas/{}'.format(isa1_uuid))
+  resp = session.get('/v1/dss/identification_service_areas/{}'.format(isa1_uuid))
   assert resp.status_code == 200
   version = resp.json()['service_area']['version']
 
   # Then delete it.
-  resp = session.delete('/identification_service_areas/{}/{}'.format(
+  resp = session.delete('/v1/dss/identification_service_areas/{}/{}'.format(
       isa1_uuid, version))
   assert resp.status_code == 200
 
 
 def test_get_deleted_isa_by_id(session, isa1_uuid):
-  resp = session.get('/identification_service_areas/{}'.format(isa1_uuid))
+  resp = session.get('/v1/dss/identification_service_areas/{}'.format(isa1_uuid))
   assert resp.status_code == 404
 
 
 def test_get_deleted_isa_by_search(session, isa1_uuid):
-  resp = session.get('/identification_service_areas?area={}'.format(
+  resp = session.get('/v1/dss/identification_service_areas?area={}'.format(
       common.GEO_POLYGON_STRING))
   assert resp.status_code == 200
   assert isa1_uuid not in [x['id'] for x in resp.json()['service_areas']]
