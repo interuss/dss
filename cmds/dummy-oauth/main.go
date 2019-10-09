b03ac96665dc724a1ea108edd8e6f24d03a29ab2
charlie-pisuraj <pcharlie@wing.com>
2019-10-09 14:54:22 -0700

    Address comments
        Wrapped getToken inside a http handler creator
        moved docker_e2e.sh into test/ and bubble up exit codes on failures

--- cmds/dummy-oauth/main.go
+++ cmds/dummy-oauth/main.go
@@ -2,75 +2,88 @@
 package main
 
 import (
-  "crypto/rsa"
-  "encoding/json"
-  "flag"
-  "fmt"
-  "io/ioutil"
-  "log"
-  "net/http"
+	"crypto/rsa"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
 
-  "github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go"
 )
 
 var (
-  address    = flag.String("addr", ":8085", "address")
-  keyFile    = flag.String("private_key_file", "config/test-certs/oauth.key", "oauth private key file")
-  privateKey *rsa.PrivateKey
+	address = flag.String("addr", ":8085", "address")
+	keyFile = flag.String("private_key_file", "config/test-certs/oauth.key", "oauth private key file")
 )
 
 // TODO(steeling): add other parameters so we can test expired tokens, invalid tokens, etc.
-func getToken(w http.ResponseWriter, r *http.Request) {
-  fmt.Println(r)
-  w.Header().Set("Content-Type", "application/json")
-  params := r.URL.Query()
+func createGetTokenHandler(privateKey *rsa.PrivateKey) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestBytes, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			fmt.Println(string(requestBytes))
+		}
 
-  aud := ""
-  auds := params["intended_audience"]
-  if len(auds) == 1 {
-    aud = auds[0]
-  }
+		w.Header().Set("Content-Type", "application/json")
+		params := r.URL.Query()
 
-  scope := ""
-  scopes := params["scope"]
-  if len(scopes) == 1 {
-    scope = scopes[0]
-  }
-  token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
-    "aud":   aud,
-    "scope": scope,
-    "sub":   "fake-user",
-  })
+		var (
+			aud  string   = ""
+			auds []string = params["intended_audience"]
+		)
+		if len(auds) == 1 {
+			aud = auds[0]
+		}
 
-  // Sign and get the complete encoded token as a string using the secret
-  // Ignore the error, it will fail the test anyways if it is not nil.
-  tokenString, err := token.SignedString(privateKey)
-  if err != nil {
-    http.Error(w, err.Error(), 500)
-    return
-  }
+		var (
+			scope  string   = ""
+			scopes []string = params["scope"]
+		)
+		if len(scopes) == 1 {
+			scope = scopes[0]
+		}
+		token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
+			"aud":   aud,
+			"scope": scope,
+			"sub":   "fake-user",
+		})
 
-  json.NewEncoder(w).Encode(map[string]string{
-    "access_token": tokenString,
-  })
+		// Sign and get the complete encoded token as a string using the secret
+		// Ignore the error, it will fail the test anyways if it is not nil.
+		tokenString, err := token.SignedString(privateKey)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		encodeError := json.NewEncoder(w).Encode(map[string]string{
+			"access_token": tokenString,
+		})
+		if encodeError != nil {
+			log.Fatal(encodeError)
+		}
+	})
 }
 
 func readPrivateKey() (*rsa.PrivateKey, error) {
-  bytes, err := ioutil.ReadFile(*keyFile)
-  if err != nil {
-    return nil, err
-  }
-  return jwt.ParseRSAPrivateKeyFromPEM(bytes)
+	bytes, err := ioutil.ReadFile(*keyFile)
+	if err != nil {
+		return nil, err
+	}
+	return jwt.ParseRSAPrivateKeyFromPEM(bytes)
 }
 
 func main() {
-  flag.Parse()
-  var err error
-  privateKey, err = readPrivateKey()
-  if err != nil {
-    log.Fatal(err)
-  }
-  http.HandleFunc("/token", getToken)
-  log.Fatal(http.ListenAndServe(*address, nil))
-
+	flag.Parse()
+	privateKey, err := readPrivateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.Handle("/token", createGetTokenHandler(privateKey))
+	log.Fatal(http.ListenAndServe(*address, nil))
 }
