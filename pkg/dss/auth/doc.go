5bda2d0efade0b035b2881e74669b90dc1d2a4a3
Thomas Voß <thomas@airmap.com>
2019-10-25 19:56:01 +0200

    [godoc] Add package description (#150)

---
+++ pkg/dss/auth/doc.go
@@ -0,0 +1,3 @@
+// Package auth bundles up functions and types used for authenticating and
+// authorizing incoming requests.
+package auth
---
+++ pkg/dss/geo/doc.go
@@ -0,0 +1,3 @@
+// Package geo provides types and functions for geospatial reasoning and
+// indexing purposes.
+package geo
