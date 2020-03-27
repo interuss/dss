dc1e3edb99264a12913681a228846f721a198110
charlie-pisuraj <56096091+charlie-pisuraj@users.noreply.github.com>
2020-03-27 00:21:55 -0700

    Move grafana dashboard out of apply-cert (#214)

--- build/apply-certs.sh
+++ build/apply-certs.sh
@@ -22,7 +22,6 @@ NODE_CERTS_DIR="$DIR/workspace/$CONTEXT/node_certs_dir"
 CA_KEY_DIR="$DIR/workspace/$CONTEXT/ca_key_dir"
 CA_CRT_DIR="$DIR/workspace/$CONTEXT/ca_certs_dir"
 JWT_PUBLIC_CERTS_DIR="$DIR/jwt-public-certs"
-GRAFANA_DASHBOARDS="$DIR/grafana-dashboards"
 UPLOAD_CA_KEY=true
 
 # Delete previous secrets in case they have changed.
@@ -34,7 +33,6 @@ kubectl delete secret cockroachdb.node --namespace "$NAMESPACE"  --context "$CON
 kubectl delete secret cockroachdb.ca.crt --namespace "$NAMESPACE"  --context "$CONTEXT" || true
 kubectl delete secret cockroachdb.ca.key --namespace "$NAMESPACE"  --context "$CONTEXT" || true
 kubectl delete secret dss.public.certs --namespace "$NAMESPACE"  --context "$CONTEXT" || true
-kubectl delete configmap grafana.dashboards --namespace "$NAMESPACE" --context "$CONTEXT" || true
 
 kubectl create secret generic cockroachdb.client.root --from-file "$CLIENTS_CERTS_DIR"  --context "$CONTEXT"
 kubectl create secret generic cockroachdb.client.root --namespace "$NAMESPACE" --from-file "$CLIENTS_CERTS_DIR"  --context "$CONTEXT"
@@ -45,5 +43,4 @@ $UPLOAD_CA_KEY && kubectl create secret generic cockroachdb.ca.key --namespace "
 # adding other operators' certificates.
 kubectl create secret generic cockroachdb.ca.crt --namespace "$NAMESPACE" --from-file "$CA_CRT_DIR"  --context "$CONTEXT"
 kubectl create secret generic dss.public.certs --namespace "$NAMESPACE" --from-file "$JWT_PUBLIC_CERTS_DIR"  --context "$CONTEXT"
-kubectl create configmap grafana.dashboards --namespace "$NAMESPACE" --from-file "$GRAFANA_DASHBOARDS" --context "$CONTEXT"
   
