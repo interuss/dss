4fd61fbb39c9e2bf21dc60017d54fe2a06e8226e
Sean Teeling <steeling@google.com>
2020-04-21 17:52:42 -0700

    Revert "factor out deprecated proto pkg"
    This reverts commit ceb71172bd8730d54d49089b7523e3f9b655002d.

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -4,11 +4,10 @@ import (
 	"context"
 	"os"
 
+	"github.com/golang/protobuf/proto"
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
 	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
-	"google.golang.org/protobuf/encoding/prototext"
-	"google.golang.org/protobuf/proto"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -106,25 +105,16 @@ func WithValuesFromContext(ctx context.Context, logger *zap.Logger) *zap.Logger 
 // logs incoming requests and corresponding responses to 'logger'.
 func DumpRequestResponseInterceptor(logger *zap.Logger) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-
-		if reqText, err := prototext.Marshal(req.(proto.Message)); err == nil {
-			logger.Sugar().Infof("Request (%s):\n%s", info.FullMethod,
-				string(reqText))
-		} else {
-			logger.Sugar().Warnf("Could not marshal text for request (%s):\n%+v",
-				info.FullMethod, req)
-		}
+		logger.Sugar().Infof("Request (%s):\n%s",
+			info.FullMethod,
+			proto.MarshalTextString(req.(proto.Message)))
 
 		resp, err = handler(ctx, req)
 
 		if resp != nil && err == nil {
-			if respText, err := prototext.Marshal(resp.(proto.Message)); err == nil {
-				logger.Sugar().Infof("Response (%s):\n%s",
-					info.FullMethod, string(respText))
-			} else {
-				logger.Sugar().Warnf("Could not marshal text for resposne (%s):\n%+v",
-					info.FullMethod, resp)
-			}
+			logger.Sugar().Infof("Response (%s):\n%s",
+				info.FullMethod,
+				proto.MarshalTextString(resp.(proto.Message)))
 		}
 		return
 	}
