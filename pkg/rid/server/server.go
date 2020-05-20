ae8d6f962ea383ef1c0ceab7e3fa70b5be4f344f
Sean Teeling <steeling@google.com>
2020-05-19 18:21:11 -0700

    refactor project structure
    add all files

--- pkg/rid/server/server.go
+++ pkg/rid/server/server.go
@@ -1,10 +1,14 @@
 package server
 
 import (
+	"context"
 	"time"
 
+	"github.com/golang/geo/s2"
+	dssmodels "github.com/interuss/dss/pkg/models"
+	ridmodels "github.com/interuss/dss/pkg/rid/models"
+
 	"github.com/interuss/dss/pkg/auth"
-	"github.com/interuss/dss/pkg/rid/application"
 )
 
 var (
@@ -25,14 +29,50 @@ var (
 	}
 )
 
+// Store provides an interface for storing DSS data.
+type Store interface {
+	// Close closes the store and should release all resources.
+	Close() error
+
+	GetISA(ctx context.Context, id dssmodels.ID) (*ridmodels.IdentificationServiceArea, error)
+
+	// Delete deletes the IdentificationServiceArea identified by "id" and owned by "owner".
+	// Returns the delete IdentificationServiceArea and all Subscriptions affected by the delete.
+	DeleteISA(ctx context.Context, id dssmodels.ID, owner dssmodels.Owner, version *dssmodels.Version) (*ridmodels.IdentificationServiceArea, []*ridmodels.Subscription, error)
+
+	// InsertISA inserts or updates an ISA.
+	InsertISA(ctx context.Context, isa *ridmodels.IdentificationServiceArea) (*ridmodels.IdentificationServiceArea, []*ridmodels.Subscription, error)
+
+	// SearchSubscriptions returns all subscriptions ownded by "owner" in "cells".
+	SearchISAs(ctx context.Context, cells s2.CellUnion, earliest *time.Time, latest *time.Time) ([]*ridmodels.IdentificationServiceArea, error)
+
+	// GetSubscription returns the subscription identified by "id".
+	GetSubscription(ctx context.Context, id dssmodels.ID) (*ridmodels.Subscription, error)
+
+	// Delete deletes the subscription identified by "id" and
+	// returns the deleted subscription.
+	DeleteSubscription(ctx context.Context, id dssmodels.ID, owner dssmodels.Owner, version *dssmodels.Version) (*ridmodels.Subscription, error)
+
+	// InsertSubscription inserts or updates a subscription.
+	InsertSubscription(ctx context.Context, s *ridmodels.Subscription) (*ridmodels.Subscription, error)
+
+	// SearchSubscriptions returns all subscriptions ownded by "owner" in "cells".
+	SearchSubscriptions(ctx context.Context, cells s2.CellUnion, owner dssmodels.Owner) ([]*ridmodels.Subscription, error)
+}
+
+// NewNilStore returns a nil Store instance.
+func NewNilStore() Store {
+	return nil
+}
+
 // Server implements ridpb.DiscoveryAndSynchronizationService.
 type Server struct {
-	App     *application.App
+	Store   Store
 	Timeout time.Duration
 }
 
 // AuthScopes returns a map of endpoint to required Oauth scope.
-func AuthScopes() map[auth.Operation][]auth.Scope {
+func (s *Server) AuthScopes() map[auth.Operation][]auth.Scope {
 	return map[auth.Operation][]auth.Scope{
 		"/ridpb.DiscoveryAndSynchronizationService/CreateIdentificationServiceArea":  {Scopes.ISA.Write},
 		"/ridpb.DiscoveryAndSynchronizationService/DeleteIdentificationServiceArea":  {Scopes.ISA.Write},
