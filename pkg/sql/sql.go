5eaf7ea4662556360861206445c4485832e0dcf4
Sean Teeling <steeling@google.com>
2020-06-09 12:43:57 -0700

    simpler

--- pkg/sql/sql.go
+++ pkg/sql/sql.go
@@ -19,40 +19,24 @@ type Queryable struct {
 	db DB
 }
 
-func getDB(ctx context.Context) (DB, error) {
+func getDB(ctx context.Context) DB {
 	dbi := ctx.Value(DBKey)
 	if dbi == nil {
 		// Return the default db
-		return db, nil
+		return db
 	}
 
-	db, ok := dbi.(DB)
-	if !ok {
-		return nil, dsserr.Internal("unknown type for db key in context: %+v", db)
-	}
-	return db, nil
+	return dbi.(DB)
 }
 
 func (q *Queryable) QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
-	db, err := getDB(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return db.QueryContext(ctx, query, args)
+	return getDB(ctx).QueryContext(ctx, query, args)
 }
 
 func (q *Queryable) QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row {
-	db, err := getDB(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return db.QueryRowContext(ctx, query, args)
+	return getDB(ctx).QueryRowContext(ctx, query, args)
 }
 
 func (q *Queryable) ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
-	db, err := getDB(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return db.ExecContext(ctx, query, args)
+	return getDB(ctx).ExecContext(ctx, query, args)
 }
