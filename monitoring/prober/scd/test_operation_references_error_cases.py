7a03e135662d396219e21a41db98dbb9c1ff37e3
Ananda Kanagaraj Sankar <sanandk@uber.com>
2020-06-22 17:40:40 -0700

    add more dss integration tests with error corner cases

---
+++ monitoring/prober/scd/test_operation_references_error_cases.py
@@ -0,0 +1,131 @@
+"""Operation References corner cases error tests:
+"""
+
+import json
+
+
+def test_op_ref_area_too_large(scd_session):
+  with open('./scd/resources/op_ref_area_too_large.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.post('/operation_references/query', json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_ref_start_end_times_past(scd_session):
+  with open('./scd/resources/op_ref_start_end_times_past.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.post('/operation_references/query', json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_ref_incorrect_units(scd_session):
+  with open('./scd/resources/op_ref_incorrect_units.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.post('/operation_references/query', json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_ref_incorrect_altitude_ref(scd_session):
+  with open('./scd/resources/op_ref_incorrect_altitude_ref.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.post('/operation_references/query', json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_uss_base_url_non_tls(scd_session, op1_uuid):
+  with open('./scd/resources/op_uss_base_url_non_tls.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_bad_keys(scd_session, op1_uuid):
+  with open('./scd/resources/op_bad_keys.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_bad_subscription_id(scd_session, op1_uuid):
+  with open('./scd/resources/op_bad_subscription.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_new_and_existing_subscription(scd_session, op1_uuid):
+  with open('./scd/resources/op_new_and_existing_subscription.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_end_time_past(scd_session, op1_uuid):
+  with open('./scd/resources/op_end_time_past.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_start_time_past(scd_session, op1_uuid):
+  with open('./scd/resources/op_start_time_past.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_already_exists(scd_session, op1_uuid):
+  with open('./scd/resources/op_request_1.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 200, resp.content
+
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 409, resp.content
+
+
+def test_op_404_version1(scd_session, op1_uuid):
+  with open('./scd/resources/op_404_version1.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 404, resp.content
+
+
+def test_op_bad_state_version0(scd_session, op1_uuid):
+  with open('./scd/resources/op_bad_state_version0.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_bad_lat_lon_range(scd_session, op1_uuid):
+  with open('./scd/resources/op_bad_lat_lon_range.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_area_too_large_put(scd_session, op1_uuid):
+  with open('./scd/resources/op_area_too_large_put.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_bad_time_format(scd_session, op1_uuid):
+  with open('./scd/resources/op_bad_time_format.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_repeated_requests(scd_session, op1_uuid):
+  with open('./scd/resources/op_request_1.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 200, resp.content
+
+  with open('./scd/resources/op_request_1.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 400, resp.content
---
+++ monitoring/prober/scd/test_operation_references_state_transition.py
@@ -0,0 +1,74 @@
+"""Operation References state transition tests:
+"""
+
+import json
+
+
+def test_op_accepted(scd_session, op1_uuid):
+  # Accepted for the first time
+  with open('./scd/resources/op_request_1.json', 'r') as f:
+    req = json.load(f)
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 200, resp.content
+
+
+def test_op_activated(scd_session, op1_uuid):
+  # GET current op
+  resp = scd_session.get('/operation_references/{}'.format(op1_uuid))
+  assert resp.status_code == 200, resp.content
+  existing_op = resp.json().get('operation_reference', None)
+  assert existing_op is not None
+
+  # Accepted to Activated
+  with open('./scd/resources/op_request_1.json', 'r') as f:
+    req = json.load(f)
+    req['state'] = 'Activated'
+    req['old_version'] = 1
+    req['key'] = [existing_op["ovn"]]
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 200, resp.content
+
+
+def test_op_accepted_bad1(scd_session, op1_uuid):
+  # GET current op
+  resp = scd_session.get('/operation_references/{}'.format(op1_uuid))
+  assert resp.status_code == 200, resp.content
+  existing_op = resp.json().get('operation_reference', None)
+  assert existing_op is not None
+
+  # Activated to Accepted with bad version number 0
+  with open('./scd/resources/op_request_1.json', 'r') as f:
+    req = json.load(f)
+    req['key'] = [existing_op["ovn"]]
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 409, resp.content
+
+
+def test_op_accepted_bad2(scd_session, op1_uuid):
+  # GET current op
+  resp = scd_session.get('/operation_references/{}'.format(op1_uuid))
+  assert resp.status_code == 200, resp.content
+  existing_op = resp.json().get('operation_reference', None)
+  assert existing_op is not None
+
+  # Activated to Accepted with valid version number
+  with open('./scd/resources/op_request_1.json', 'r') as f:
+    req = json.load(f)
+    req['state'] = 'Accepted'
+    req['old_version'] = 2
+    req['key'] = [existing_op["ovn"]]
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 400, resp.content
+
+
+def test_op_bad_state_transition(scd_session, op1_uuid):
+  # Delete operation
+  resp = scd_session.delete('/operation_references/{}'.format(op1_uuid))
+  assert resp.status_code == 200, resp.content
+
+  # Create operation with Closed state
+  with open('./scd/resources/op_request_1.json', 'r') as f:
+    req = json.load(f)
+    req['state'] = 'Closed'
+  resp = scd_session.put('/operation_references/{}'.format(op1_uuid), json=req)
+  assert resp.status_code == 400, resp.content
