7626958703977223e44fd6189e04f864ced3f5d9
Dos-Ph <14473248+Dos-Ph@users.noreply.github.com>
2020-07-07 18:08:41 -0700

    Add flag to GRPC Backend for self identification
    Signed-off-by: Dos-Ph <14473248+Dos-Ph@users.noreply.github.com>

--- cmds/grpc-backend/main.go
+++ cmds/grpc-backend/main.go
@@ -52,6 +52,7 @@ var (
 	dumpRequests      = flag.Bool("dump_requests", false, "Log request and response protos")
 	profServiceName   = flag.String("gcp_prof_service_name", "", "Service name for the Go profiler")
 	enableSCD         = flag.Bool("enable_scd", false, "Enables the Strategic Conflict Detection API")
+	locality          = flag.String("locality", "", "self-identification string used as CRDB table writer column")
 
 	cockroachParams = struct {
 		host            *string
