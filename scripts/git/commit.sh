80586ecc1006fbf4e81867639a462b91a1c7d708
Thomas Voß <me@tvoss.io>
2020-07-15 11:02:00 +0200

    Silence git cherry error messages

--- scripts/git/commit.sh
+++ scripts/git/commit.sh
@@ -2,7 +2,7 @@
 
 COMMIT=$(git rev-parse --short HEAD)
 
-if [[ -n $(git status -s) ]] || [[ -n $(git cherry) ]]; then 
+if [[ -n $(git status -s) ]] || [[ -n $(git cherry 2> /dev/null) ]]; then 
     echo ${COMMIT}-dirty
 else 
     echo ${COMMIT}
