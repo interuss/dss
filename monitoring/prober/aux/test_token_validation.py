0bdc31823fed46022f147b85a3c346815da4f0bc
Benjamin Pelletier <BenjaminPelletier@users.noreply.github.com>
2020-07-21 06:43:32 -0700

    Clean up issues found during deployment projects (#364)

--- monitoring/prober/aux/test_token_validation.py
+++ monitoring/prober/aux/test_token_validation.py
@@ -1,23 +1,27 @@
-"""Test Authentication validation
-"""
+"""Test aux features"""
+
+import pytest
 
 from ..infrastructure import default_scope
+from ..auth import DummyOAuth
+from ..rid.common import SCOPE_READ as RID_SCOPE_READ
 
 
-@default_scope('dss.read.identification_service_areas')
+@default_scope(RID_SCOPE_READ)
 def test_validate(aux_session):
   resp = aux_session.get('/validate_oauth')
   assert resp.status_code == 200
 
 
-@default_scope('dss.read.identification_service_areas')
+@default_scope(RID_SCOPE_READ)
 def test_validate_token_good_user(aux_session):
-  # TODO: These tests should work with any AuthAdapter, not just the dummy OAuth.  Hardcoding fake_uss here won't work in general.
+  if not isinstance(aux_session.auth_adapter, DummyOAuth):
+    pytest.skip('User ID is not known for general auth providers')
   resp = aux_session.get('/validate_oauth?owner=fake_uss')
   assert resp.status_code == 200
 
 
-@default_scope('dss.read.identification_service_areas')
+@default_scope(RID_SCOPE_READ)
 def test_validate_token_bad_user(aux_session):
   resp = aux_session.get('/validate_oauth?owner=bad_user')
   assert resp.status_code == 403
