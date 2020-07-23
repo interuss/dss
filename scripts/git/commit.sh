09ca064082e61642d87d305347cde4e0be7b7ce0
Benjamin Pelletier <pelletierb@wing.com>
2020-07-23 09:57:18 -0700

    Improve deployment experience

--- scripts/git/commit.sh
+++ scripts/git/commit.sh
@@ -2,8 +2,11 @@
 
 COMMIT=$(git rev-parse --short HEAD)
 
-if  [[ -n "$(git status -s)" ]] || [[ -n "$(git cherry 2> /dev/null)" ]]; then
+if  [[ -n "$(git status -s)" ]]; then
     echo ${COMMIT}-dirty
+elif [[ -n "$(git cherry 2> /dev/null)" ]]; then
+    echo ${COMMIT}-localcommit
+#TODO: Handle the case where the current branch does not exist remotely
 else
     echo ${COMMIT}
 fi
