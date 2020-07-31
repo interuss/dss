3125cac376eeaa687d84a66c98d547cb9b1588e1
charlie-pisuraj <pcharlie@wing.com>
2020-07-31 16:52:25 -0700

    Fix SQL statements
    Constraints name must be unique and applying the constraints to upgrade from old V2 where bootstrap scripts was in GRPC we needed to rename the constraints the make the run valid

--- build/deploy/db_schemas/defaultdb/000002_inverted_index.up.sql
+++ build/deploy/db_schemas/defaultdb/000002_inverted_index.up.sql
@@ -1,6 +1,4 @@
-ALTER TABLE identification_service_areas ADD COLUMN IF NOT EXISTS cells INT64[] NOT NULL;
-ALTER TABLE identification_service_areas ADD CONSTRAINT cells_not_null CHECK (array_length(cells, 1) IS NOT NULL);
+ALTER TABLE identification_service_areas ADD COLUMN IF NOT EXISTS cells INT64[];
 CREATE INVERTED INDEX IF NOT EXISTS cell_idx on identification_service_areas (cells);
-ALTER TABLE subscriptions ADD COLUMN IF NOT EXISTS cells INT64[] NOT NULL;
-ALTER TABLE subscriptions ADD CONSTRAINT cells_not_null CHECK (array_length(cells, 1) IS NOT NULL);
+ALTER TABLE subscriptions ADD COLUMN IF NOT EXISTS cells INT64[];
 CREATE INVERTED INDEX IF NOT EXISTS cell_idx on subscriptions (cells);
--- build/deploy/db_schemas/defaultdb/000005_bump_version.down.sql
+++ build/deploy/db_schemas/defaultdb/000005_bump_version.down.sql
@@ -1,1 +1,1 @@
-UPDATE schema_versions set schema_version = 'v2.0.0' WHERE onerow_enforcer = TRUE
+UPDATE schema_versions set schema_version = 'v2.0.0' WHERE onerow_enforcer = TRUE;
