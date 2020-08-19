2542aa18a0278c03d2531a95b2bef3f1f25e6592
Thomas Voß <me@tvoss.io>
2020-08-19 14:45:49 +0200

    [build] Fix git commit script

--- monitoring/prober/aux_/test_version.py
+++ monitoring/prober/aux_/test_version.py
@@ -7,4 +7,4 @@ def test_version(aux_session):
   assert resp.status_code == 200
   version = resp.json()['version']['as_string']
   assert version
-  assert 'undefined' not in version, version
+  assert '0.0.0-undefined' not in version, version
