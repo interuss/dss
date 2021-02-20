ceeeb9a42eca825e88727b0ac28c8c4d58e8a747
Benjamin Pelletier <BenjaminPelletier@users.noreply.github.com>
2021-02-20 12:13:48 -0800

    Update CRDB version to 20.2.0 in non-production usages (#477)

--- test/docker_e2e.sh
+++ test/docker_e2e.sh
@@ -66,7 +66,7 @@ echo "Starting cockroachdb with admin port on :8080"
 docker run -d --rm --name dss-crdb-for-debugging \
 	-p 26257:26257 \
 	-p 8080:8080 \
-	cockroachdb/cockroach:v20.1.1 start \
+	cockroachdb/cockroach:v20.2.0 start-single-node \
 	--insecure > /dev/null
 
 sleep 1
