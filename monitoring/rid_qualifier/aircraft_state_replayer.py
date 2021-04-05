05be82561266a2235391aa087537442499ec9f1b
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-04-05 22:33:50 +0100

    Create state_replayer file and update README, update requirements.txt

---
+++ monitoring/rid_qualifier/aircraft_state_replayer.py
@@ -0,0 +1,8 @@
+import requests
+
+class TestHarnessSubmitter():
+    ''' A class to read Aircraft RID State files from disk and submit flight tracks to the test harness / endpoint of individual UTM SP '''
+
+
+if __name__ == '__main__':
+    pass
