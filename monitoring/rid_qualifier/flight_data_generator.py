5f46a75f3059624ed575ea557371f2a169fc5c52
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-05-03 15:20:06 +0100

    Update flight_data_generator.py

--- monitoring/rid_qualifier/flight_data_generator.py
+++ monitoring/rid_qualifier/flight_data_generator.py
@@ -3,6 +3,7 @@ import shapely.geometry
 from pyproj import Geod, Transformer, Proj
 import json
 from pathlib import Path
+from typing import List, NamedTuple, Any
 import arrow
 import datetime
 from datetime import datetime, timedelta
