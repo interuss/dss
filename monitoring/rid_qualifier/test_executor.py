93f7683d10e654e4e2b29d2a6da5d94b9cd55792
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-05-19 14:48:53 +0100

    Update test_executor.py

--- monitoring/rid_qualifier/test_executor.py
+++ monitoring/rid_qualifier/test_executor.py
@@ -32,9 +32,3 @@ async def main(test_configuration: RIDQualifierTestConfiguration):
     # TODO: call display data evaluator to read RID system state and compare to expectations
 
 
-
-if __name__ == '__main__':
-    uss_config = build_uss_config( injection_base_url="https://dss.unmanned.corp", allocated_track=0)
-    test_configuration = build_test_configuration(locale='che', auth_spec="DummyOAuth(http://localhost:8085/token, sub=uss1)",uss_config = uss_config)
-    
-    asyncio.run(main(test_configuration=test_configuration))
