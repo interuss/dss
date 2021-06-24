e457bfab52a3a2e037cb1b06d28565a2db4694db
Pratibha <pratibhag@google.com>
2021-06-24 22:19:11 +1000

    Lat,Lng adjustments

--- monitoring/rid_qualifier/create_flight_record_from_kml.py
+++ monitoring/rid_qualifier/create_flight_record_from_kml.py
@@ -17,7 +17,8 @@ def get_flight_details():
 def get_flight_coordinates():
     # TODO: To be fetched from KML.
     # Hardcoded for now.
-    return input_coordinates
+    # Reverse the Lng,Lat from KML to Lat,Lng for processing.
+    return [(p[1], p[0], p[2]) for p in input_coordinates]
 
 
 def get_distance_travelled():
@@ -35,8 +36,16 @@ def check_if_vertex_is_correct(point1, point2, point3, flight_distance):
     x1, y1 = point1
     x2, y2 = point2
     x3, y3 = point3
-    points_distance_difference = math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) - math.sqrt((x2-x3)*(x2-x3) + (y2-y3)*(y2-y3))
-    assert abs(points_distance_difference - flight_distance) < 1, 'Generated vertex is not correct.'
+    points_distance_difference = math.sqrt(
+        (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) - math.sqrt((x2-x3)*(x2-x3) + (y2-y3)*(y2-y3))
+    assert abs(
+        points_distance_difference - flight_distance
+        ) < 0.2, f'Generated vertex is not correct. {point1}, {point2}, {point3}, {flight_distance}'
+
+
+def test_distance_bw_two_points(last_flight_state, state_vertex):
+    distance_bw_last_two_states = get_distance_between_two_points(last_flight_state, state_vertex)
+    assert distance_bw_last_two_states < 1.9, f'Points are not equally positioned.{last_flight_state}, {state_vertex}'
 
 
 def get_flight_state_vertices(flatten_points):
@@ -52,15 +61,35 @@ def get_flight_state_vertices(flatten_points):
     flight_distance = get_distance_travelled()
     flight_state_vertices = []
     while True:
-        v_distance = get_distance_between_two_points(point1, point2)
-        if v_distance == flight_distance or abs(v_distance-flight_distance) < 1:
+        input_coord_gap = get_distance_between_two_points(point1, point2)
+        if input_coord_gap <= 0:
+            # points are overlapping
+            point1 = point2
+            point2 = next(points, None)
+            if not point2:
+                break
+            continue
+        if input_coord_gap == flight_distance:
             flight_state_vertices.append(point2)
             point1 = point2
             point2 = next(points, None)
             if not point2:
                 break
-        if flight_distance > v_distance:
-            remaining_flight_distance = flight_distance - v_distance
+        if flight_distance < input_coord_gap:
+            remaining_flight_distance = input_coord_gap
+            while remaining_flight_distance > flight_distance:
+                state_vertex = get_vertex_between_points(point1, point2, flight_distance)
+                if state_vertex:
+                    state_vertex = state_vertex.coords[:][0]
+                    # TODO: move it to unit tests.
+                    check_if_vertex_is_correct(point1, point2, state_vertex, flight_distance)
+                    flight_state_vertices.append(state_vertex)
+                    point1 = state_vertex
+                    remaining_flight_distance -= flight_distance
+            if remaining_flight_distance > 0:
+                input_coord_gap = remaining_flight_distance
+        if flight_distance > input_coord_gap:
+            remaining_flight_distance = flight_distance - input_coord_gap
             point1 = point2
             point2 = next(points, None)
             if not point2:
@@ -70,14 +99,13 @@ def get_flight_state_vertices(flatten_points):
             if state_vertex:
                 state_vertex = state_vertex.coords[:][0]
                 flight_state_vertices.append(state_vertex)
+                if state_vertex == point2:  # This is the special case when remaining_distance is very close to flight_distance
+                    point2 = next(points, None)
+                    if not point2:
+                        flight_state_vertices.append(point1)
+                        break
                 point1 = state_vertex
-        if flight_distance < v_distance:
-            state_vertex = get_vertex_between_points(point1, point2, flight_distance)
-            if state_vertex:
-                state_vertex = state_vertex.coords[:][0]
-                check_if_vertex_is_correct(point1, point2, state_vertex, flight_distance)
-                flight_state_vertices.append(state_vertex)
-                point1 = state_vertex
+        
     return flight_state_vertices
 
 
@@ -98,8 +126,8 @@ def get_vertex_between_points(point1, point2, at_distance):
 def main():
     coordinates = get_flight_coordinates()
     reference_point = coordinates[0] # TODO: Check `if coordinates:`.
-    # alt = reference_point[2] # TODO: Get alt from coordinates.
-    alt = '140'
+    alt = reference_point[2] # TODO: Get alt from coordinates.
+    alt = 140
     flatten_points = []
     for point in coordinates:
         flatten_points.append(flatten(
@@ -108,10 +136,12 @@ def main():
         ))
     
     flight_state_vertices = get_flight_state_vertices(flatten_points)
-
     flight_state_vertices_unflatten = [unflatten(s2sphere.LatLng.from_degrees(*reference_point[:2]), v) for v in flight_state_vertices]
+
     # TODO: Fetch Lat,Lng values from s2sphere.LatLng object without converting it to string.
-    flight_state_vertices_unflatten = [str(p).lstrip('LatLng: ')+f',{alt}' for p in flight_state_vertices_unflatten]
+    # Position Lat, Lng to Lng, Lat order for KML representation.
+    flight_state_vertices_unflatten = [(str(p.lng().degrees), str(p.lat().degrees), str(alt)) for p in flight_state_vertices_unflatten]
+    flight_state_vertices_unflatten = [','.join(p) for p in flight_state_vertices_unflatten]
     flight_state_vertices_str = '\n'.join(flight_state_vertices_unflatten)
     with open("monitoring/rid_qualifier/test_data/flight_state_coordinates.txt", "w") as text_file:
         text_file.write(flight_state_vertices_str)
