1dae1b65753daca914d5a25c2f7c8ccab3409734
Pratibha <pratibhag@google.com>
2021-07-27 10:17:58 +1000

    cleanup

--- monitoring/rid_qualifier/host/tasks.py
+++ monitoring/rid_qualifier/host/tasks.py
@@ -7,13 +7,6 @@ from monitoring.rid_qualifier import test_executor
 from monitoring.rid_qualifier.utils import RIDQualifierTestConfiguration
 from monitoring.monitorlib.typing import ImplicitDict
 
-def example(seconds):
-    print('Starting task')
-    for i in range(seconds):
-        print(i)
-        time.sleep(1)
-    print('Task completed')
-    return 'task completed'
 
 def get_rq_job(job_id):
   try:
@@ -22,11 +15,6 @@ def get_rq_job(job_id):
       return None
   return rq_job
 
-# def call_test_executor(user_config_json, auth_spec):
-#     user_config: RIDQualifierTestConfiguration = ImplicitDict.parse(
-#         json.loads(user_config_json), RIDQualifierTestConfiguration)
-#     job = config.Config.qualifier_queue.enqueue(test_executor.main, user_config, auth_spec)
-#     return job.get_id()
 
 def call_test_executor(user_config_json, auth_spec):
     user_config: RIDQualifierTestConfiguration = ImplicitDict.parse(
