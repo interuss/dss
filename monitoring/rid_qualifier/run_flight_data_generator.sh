12a35609a21b79efdb612d087c4fa5b564467ec0
Michael Barroco <michael@orbitalize.com>
2021-07-31 09:11:35 +0200

    Add script to run flight_data_generator.py in the docker container

---
+++ monitoring/rid_qualifier/run_flight_data_generator.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Find and change to repo root directory
+OS=$(uname)
+if [[ "$OS" == "Darwin" ]]; then
+	# OSX uses BSD readlink
+	BASEDIR="$(dirname "$0")"
+else
+	BASEDIR=$(readlink -e "$(dirname "$0")")
+fi
+cd "${BASEDIR}/../.." || exit 1
+
+docker build \
+    -f monitoring/rid_qualifier/Dockerfile \
+    -t interuss/dss/rid_qualifier \
+    --build-arg version=`scripts/git/commit.sh` \
+    monitoring
+
+docker run -i -t --name flight_data_generator \
+  --rm \
+  --tty \
+  -e PYTHONBUFFERED=1 \
+  -v $(pwd)/monitoring/rid_qualifier/test_definitions:/app/test_definitions \
+  interuss/dss/rid_qualifier \
+  python flight_data_generator.py
