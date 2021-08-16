b719c2ae3cd6a028558774bbbf35974391f6569c
Pratibha <pratibhag@google.com>
2021-08-17 07:30:00 +1000

    working tests page

--- monitoring/rid_qualifier/host/forms.py
+++ monitoring/rid_qualifier/host/forms.py
@@ -1,6 +1,6 @@
 import json
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, BooleanField, IntegerField
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, IntegerField, widgets, SelectMultipleField
 from wtforms.validators import DataRequired, ValidationError
 from wtforms.widgets import HiddenInput
 
@@ -21,3 +21,34 @@ class UserConfig(FlaskForm):
         if int(form.file_count.data) < len(user_config['injection_targets']):
             raise ValidationError(
                 'Not enough flight states files provided for each injection_targets.')
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+class SimpleForm(FlaskForm):
+    # string_of_files = ['one\r\ntwo\r\nthree\r\n']
+    # list_of_files = string_of_files[0].split()
+    # create a list of value/description tuples
+    # files = StringField(widget=HiddenInput())
+    # files_list = str(files.data).split(',')
+    # files = [(x, x) for x in files_list]
+    example = MultiCheckboxField('Label', choices=[])
+
+class TestsExecuteForm(FlaskForm):
+    flight_records = MultiCheckboxField('Flight Records', choices=[], validators=[DataRequired()])
+    auth_spec = StringField('Auth Spec', validators=[DataRequired()])
+    user_config = TextAreaField('User Config', validators=[DataRequired()])
+    sample_report = BooleanField('Sample Report')
+    submit = SubmitField('Submit')
+
+    def validate_user_config(form, field):
+        user_config = json.loads(field.data)
+        expected_keys = {'injection_targets', 'observers'}
+        if not expected_keys.issubset(set(user_config)):
+            message = f'missing fields in config object {expected_keys - set(user_config)}'
+            raise ValidationError(message)
+        if len(form.flight_records.data) < len(user_config['injection_targets']):
+            raise ValidationError(
+                'Not enough flight states files provided for each injection_targets.')
+    # def validate_flight_records(form, field):
--- monitoring/rid_qualifier/host/routes.py
+++ monitoring/rid_qualifier/host/routes.py
@@ -8,6 +8,7 @@ from . import config
 from . import forms
 from . import tasks
 
+from datetime import datetime
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 import google.auth.transport.requests
@@ -98,6 +99,67 @@ def start_background_task(user_config, auth_spec, input_files, debug):
     return job.get_id()
 
 
+# @webapp.route('/tests')
+# @login_required
+# def tests():
+#     data = get_flight_records()
+#     return render_template('tests.html', data=data)
+
+@webapp.route('/tests', methods=['GET', 'POST'])
+@login_required
+def tests():
+    flight_record_data = get_flight_records()
+    test_history = get_test_history()
+    files = []
+    if flight_record_data.get('flight_records'):
+        files= [(x, x) for x in flight_record_data['flight_records']]
+    else:
+        return 'Flight records not available.'
+    form = forms.TestsExecuteForm()
+    form.flight_records.choices = files
+    data = get_test_history()
+    if form.validate_on_submit():
+        file_objs = []
+        input_files_location = f'{config.Config.FILE_PATH}/user_name/flight_records'
+        for filename in form.flight_records.data:
+            logging.info(f'file: {filename}')
+            print('file 123: ', filename)
+            filepath = f'{input_files_location}/{filename}'
+            with open(filepath) as fo:
+                file_objs.append(fo.read())
+            job_id = start_background_task(
+                form.user_config.data,
+                form.auth_spec.data,
+                file_objs,
+                form.sample_report.data)
+    #   form_status = 'submitted'
+    if request.method == 'POST':
+        data.update({
+            'job_id': job_id
+        })
+    return render_template(
+        'tests.html',
+        title='Execute tests',
+        form=form,
+        data=data)
+
+# @webapp.route('/flight-records', methods=['GET', 'POST'])
+def get_flight_records():
+    data = {
+        'flight_records': [],
+        'message': ''
+    }
+    folder_path = f'{config.Config.FILE_PATH}/user_name/flight_records'
+    if not os.path.isdir(folder_path):
+        data['message'] = 'Flight records not available.'
+    else:
+        flight_records = [f for f in os.listdir(folder_path) if f.endswith('.json')]
+        data['flight_records'] = flight_records
+    # return render_template('flight_records.html', data=data)
+    # return render_template('tests2.html', flight_data=data)
+    return data
+    
+
 @webapp.route('/executor', methods=['GET', 'POST'])
 @login_required
 def execute_task():
@@ -140,15 +202,16 @@ def get_result(job_id):
             'task_result': task.result,
         }
     if task.get_status() == 'finished':
-      if task.result:
-        filename = 'report.json'
-        filepath = f'{config.Config.FILE_PATH}/user_name/tests/{filename}'
-        os.makedirs(os.path.dirname(filepath), exist_ok=True)
-        with open(filepath, 'w') as f:
-          f.write(task.result)
-        response_object.update({'filename': filename})
-      else:
-        logging.info('Task result not available yet..')
+        now = datetime.now()
+        if task.result:
+            filename = f'{str(now.date())}_{now.strftime("%H%M%S")}.json'
+            filepath = f'{config.Config.FILE_PATH}/user_name/tests/{filename}'
+            os.makedirs(os.path.dirname(filepath), exist_ok=True)
+            with open(filepath, 'w') as f:
+                f.write(task.result)
+            response_object.update({'filename': filename})
+        else:
+            logging.info('Task result not available yet..')
     return response_object
 
 
@@ -160,6 +223,42 @@ def get_report(job_id):
     output.headers['Content-type'] = 'text/csv'
     return output
 
+@webapp.route('/test_download/<string:filename>', methods=['POST'])
+def download_test(filename):
+    filepath = f'{config.Config.FILE_PATH}/user_name/tests/{filename}'
+    # os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    content = ''
+    with open(filepath) as f:
+        content = f.read()
+    if content:
+        output = make_response(content)
+        output.headers['Content-Disposition'] = 'attachment; filename=report.json'
+        output.headers['Content-type'] = 'text/csv'
+        return output
+    return {'error': 'Error downloading file'}
+
+# @webapp.route('/history')
+def get_test_history():
+    output_path = f'{config.Config.FILE_PATH}/user_name/tests'
+    try:
+        executed_tests = os.listdir(output_path)
+    except:
+        executed_tests = []
+    return {'tests': executed_tests}
+
+@webapp.route('/simple',methods=['post','get'])
+def hello_world():
+    forms.FILES = ['1', '2', '3']
+    form = forms.SimpleForm()
+    form.example.choices = [('ONE', 'one'), ('TWO', 'two')]
+    # form.example.data = ['file1', 'file2', 'file3']
+    if form.validate_on_submit():
+        print(form.example.data)
+        return render_template("success.html", data=form.example.data)
+    else:
+        print("Validation Failed")
+        print(form.errors)
+    return render_template('example.html',form=form)
 
 @webapp.route('/upload_file', methods=['POST'])
 def upload_flight_state_files():
@@ -176,10 +275,11 @@ def upload_flight_state_files():
                 file_path = os.path.join(folder_path, filename)
                 file.save(file_path)
                 destination_file_paths.append(file_path)
-    return redirect(
-        url_for(
-            '.execute_task',
-            files=','.join(destination_file_paths)))
+    # return redirect(
+    #     url_for(
+    #         '.execute_task',
+    #         files=','.join(destination_file_paths)))
+    return redirect(url_for('.tests'))
 
 
 @webapp.route('/status')
