678e0fda6c0fb99a6462e9542266c49fdf414a1c
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-10-06 22:28:57 +0100

    Updated output directories and added docstrings to the utils file

--- monitoring/scd_qualifier/utils.py
+++ monitoring/scd_qualifier/utils.py
@@ -1,9 +1,8 @@
-from typing import List, NamedTuple
-
+from typing import List
+from geojson import Feature
 from shapely.geometry.linestring import LineString
 from monitoring.monitorlib.typing import ImplicitDict
 import arrow
-import uuid
 
 class StringBasedDateTime(str):
   """String that only allows values which describe a datetime."""
@@ -17,33 +16,33 @@ class StringBasedDateTime(str):
     return str_value
 
 class LatLngPoint(ImplicitDict):
-    '''A clas to hold information about a location as Latitude / Longitude pair'''
+    '''A class to hold information about a location as Latitude / Longitude pair'''
     lat: float
     lng: float
 
 class Radius(ImplicitDict):
-    ''' A class to hold the radius of a circle for the outline_circle object '''
+    ''' A class to hold the radius of a circle for the outline_circle object as specified per the ASTM standard '''
     value: float
     units:str
 
 class VolumePolygon(ImplicitDict):
-    ''' A class to hold the polygon object, used in the outline_polygon of the Volume3D object '''
+    ''' A class to hold the polygon object, used in the outline_polygon of the Volume3D object as specified in the ASTM standard '''
     vertices: List[LatLngPoint] # A minimum of three LatLngPoints
 
 class Circle(ImplicitDict):
-    ''' Hold the details of a circle object used in the outline_circle object'''
+    ''' Hold the details of a circle object used in the outline_circle object as specified in the ASTM standard '''
     center: LatLngPoint 
     radius: Radius
 
 
 class Altitude(ImplicitDict):
-    ''' A class to hold altitude '''
+    ''' A class to hold altitude per ASTM standard '''
     value:int
     reference:str 
     units: str 
 
 class Time(ImplicitDict):
-    ''' A class to hold Time'''
+    ''' A class to hold Time per ASTM standard'''
     value: str
     format:str 
 
@@ -51,9 +50,8 @@ class OperationalIntentReference(ImplicitDict):
     """Class for keeping track of an operational intent reference"""
     id: str
 
-
 class Volume3D(ImplicitDict):
-    '''A class to hold Volume3D objects'''
+    '''A class to hold Volume3D objects '''
     
     outline_polygon: VolumePolygon
     altitude_lower: Altitude
@@ -67,7 +65,7 @@ class Volume4D(ImplicitDict):
 
 
 class OperationalIntentDetails(ImplicitDict):
-    """Class for keeping track of an operational intent reference """
+    """Class for holding details of an operational intent reference """
     volumes: List[Volume4D]
     priority: int
 
@@ -77,12 +75,25 @@ class VolumeGenerationRules(ImplicitDict):
     intersect_time: bool = 0
     is_control: bool = 0
     expected_result: str
+
+class GeoJSONFeature(ImplicitDict):
+    ''' A class to hold GeoJSON Feature flight plan, at this moment we are using a LineString but it could be a LineString or Polygon '''
+    geojson: Feature
+    
+class SCDVolume4D(ImplicitDict):
+    ''' A class to hold a volume 4D with path and time details and GeoJSON flight plan, USSP will have to transform flight plan to a  '''
+    flight_plan: GeoJSONFeature
+    time_start: StringBasedDateTime
+    time_end: StringBasedDateTime
+    altitude_lower: Altitude
+    altitude_upper: Altitude
     
 class PathGenerationRules(ImplicitDict):
-    """ A class to hold configuration for developing treatment flight path as GeoJSON """
+    """ A class to hold configuration for developing treatment flight path """
     intersect_space:bool = 0
     
 class PathPayload(ImplicitDict):
+    ''' An object to hold generated flight path, is_control is a nomenclature used to see if the generated path is the first one '''
     path: LineString
     is_control: bool
     path_options: PathGenerationRules
