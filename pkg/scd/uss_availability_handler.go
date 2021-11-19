3b06075e1d943183346f8ad34ac75eca109f879f
Pratibha <pratibhag@google.com>
2021-11-19 13:42:49 +1100

    pytest placeholders for uss_availability check

--- pkg/scd/uss_availability_handler.go
+++ pkg/scd/uss_availability_handler.go
@@ -1,12 +1,14 @@
 package scd
-
+import "log"
 import "context"
 import "github.com/interuss/dss/pkg/api/v1/scdpb"
 
 func (a *Server) GetUssAvailability(ctx context.Context, request *scdpb.GetUssAvailabilityRequest) (*scdpb.UssAvailabilityStatusResponse, error) {
-	panic("implement me")
+	log.Println("implement me")
+	return nil, nil
 }
 
 func (a *Server) SetUssAvailability(ctx context.Context, request *scdpb.SetUssAvailabilityRequest) (*scdpb.UssAvailabilityStatusResponse, error) {
-	panic("implement me")
+	log.Println("implement me")
+	return nil, nil
 }
