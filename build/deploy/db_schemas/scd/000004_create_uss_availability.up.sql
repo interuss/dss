c8f0086c6e08ae93d1aa74c53e6184f5573fc60a
Pratibha <pratibhag@google.com>
2021-12-13 10:32:26 +1100

    merging 630

--- build/deploy/db_schemas/scd/000004_create_uss_availability.up.sql
+++ build/deploy/db_schemas/scd/000004_create_uss_availability.up.sql
@@ -3,3 +3,6 @@ CREATE TABLE IF NOT EXISTS scd_uss_availability (
   availability STRING NOT NULL,
   updated_at TIMESTAMPTZ NOT NULL
 );
+
+/* Update database version */
+UPDATE schema_versions set schema_version = 'v3.1.0' WHERE onerow_enforcer = TRUE;
