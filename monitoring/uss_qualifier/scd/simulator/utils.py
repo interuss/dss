893e61df684fc8303ca65821f91806c519f98621
Hrishikesh Ballal <hballal@gmail.com>
2022-01-05 20:13:36 +0000

    Update utils.py

--- monitoring/uss_qualifier/scd/simulator/utils.py
+++ monitoring/uss_qualifier/scd/simulator/utils.py
@@ -5,17 +5,17 @@ from pathlib import Path
 from monitoring.monitorlib.scd_automated_testing.scd_injection_api import InjectFlightRequest
 
 class GeometryGenerationRule(ImplicitDict):
-    """ A class to hold configuration for developing treatment flight path """
+    """ A class to hold configuration for developing flight paths for testing """
     intersect_space:bool = 0
     
 class GeneratedGeometry(ImplicitDict):
-    ''' An object to hold generated flight path, is_control is a nomenclature used to see if the generated path is the first one '''
+    ''' An object to hold generated flight path and the associated rule '''
     geometry: Union[LineString, Polygon]    
     geometry_generation_rule: GeometryGenerationRule
   
 class RequiredResults(ImplicitDict):
     ''' A class to evaluate results / response to an injection of test flight data (TestFlightRequest) '''
-    expected_response: str # One of "Planned", "Rejected", "ConflictWithFlight" or "Failed"
+    expected_response: str 
     authorisation_data_fields_to_evaluate: List[str]
     operational_intent_processing_result: str
 
