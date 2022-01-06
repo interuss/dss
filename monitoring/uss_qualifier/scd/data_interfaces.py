11a552d654bbdb39f13e57cfc14dcc9ea9a49a5f
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2022-01-06 17:41:39 +0000

    Update data_interfaces.py

--- monitoring/uss_qualifier/scd/data_interfaces.py
+++ monitoring/uss_qualifier/scd/data_interfaces.py
@@ -2,13 +2,10 @@ from typing import List
 from monitoring.monitorlib.typing import ImplicitDict
 from monitoring.monitorlib.scd_automated_testing.scd_injection_api import InjectFlightRequest
 
-
-
 class RequiredResults(ImplicitDict):
     ''' A class to evaluate results / response to an injection of test flight data (TestFlightRequest) '''
-    expected_response: str 
-    authorisation_data_fields_to_evaluate: List[str]  # In the 
-    operational_intent_processing_result: str
+    expected_result: str 
+    # Why we expected a response of rejected (add a dict / enum)
 
 class TestInjectionRequiredResult(ImplicitDict):
     test_injection: InjectFlightRequest
