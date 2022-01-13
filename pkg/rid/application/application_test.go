1e78e408e5ead6d5c5e33ad7e772bea1dc960730
Pratibha <pratibhag@google.com>
2022-01-14 07:20:08 +1100

    build issues

--- pkg/rid/application/application_test.go
+++ pkg/rid/application/application_test.go
@@ -23,7 +23,7 @@ import (
 
 var (
 	storeURI  = flag.String("store-uri", "", "URI pointing to a Cockroach node")
-	fakeClock = clockwork.NewFakeClockAt(time.Now())
+	fakeClock = clockwork.NewFakeClock()
 	startTime = fakeClock.Now().Add(-time.Minute)
 	endTime   = fakeClock.Now().Add(time.Hour)
 )
