95bd797d4d8755cb15415abf0104a54e0fb2b3c4
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2022-01-20 16:23:23 +0000

    Update data_interfaces.py

--- monitoring/uss_qualifier/scd/data_interfaces.py
+++ monitoring/uss_qualifier/scd/data_interfaces.py
@@ -26,19 +26,29 @@ class KnownIssueFields(ImplicitDict):
 class KnownResponses(ImplicitDict):
     ''' A class to evaluate results / response to an injection of test flight data (TestFlightRequest). This data structure holds information about the "result" response provided by the test interface of the USS under test and details of how long the executor should wait for a response. Per the SCD Testing API an acceptable result should be one of "Planned", "Rejected", "ConflictWithFlight" or "Failed". In the case where the USS under test provides a result that is not the same as expected result then the test driver can provide a message detailing why the expected result was populated the way it was. This should help the USS under test debug specific parts of their internal systems. '''
     acceptable_results: List[str] 
+    ''' Acceptable strings that the USS under test can report as the result of processing the test data. '''
+
     incorrect_result_details: Dict[str, KnownIssueFields]
-    
+    ''' In cases where the USS provides a response that is not in the acceptable results, the test exceutor may display a message to the user detailing why the reported response was not correct '''
 
 class InjectionTarget(ImplicitDict):
     ''' A class to hold the role of the USS under test '''
     uss_role: str
+    ''' In some testing scenarios, the USS may be assigned a role e.g. Querying USS based on the actions they will perform in the test scenario '''
 
 class FlightInjectionAttempt(ImplicitDict):
     ''' A class to hold details of the test injection, the injection target and the expected result of processing the flight request '''
     test_injection: InjectFlightRequest
+    ''' Data around flight authorisation and operational intents that are submitted to the test interface of the USS under test '''
+
     known_responses: KnownResponses
+    ''' Details about what the USS under test should report after processing the test data '''
+
     injection_target: InjectionTarget
+    ''' Details of the USS under test as mapped to the test type '''
 
 class AutomatedTest(ImplicitDict):
     ''' A class to hold injection attempts and test definitions '''
+
     injection_attempts: List[FlightInjectionAttempt]
+    ''' Details of attempts of submitting test data to the interface of USS under test '''
