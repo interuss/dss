0b6b6e97ddf7dfacd63bae8b60f4b0d5d687081f
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2022-01-20 12:15:13 +0000

    Update data_interfaces.py

--- monitoring/uss_qualifier/scd/data_interfaces.py
+++ monitoring/uss_qualifier/scd/data_interfaces.py
@@ -3,10 +3,6 @@ from monitoring.monitorlib.typing import ImplicitDict
 from monitoring.monitorlib.scd_automated_testing.scd_injection_api import InjectFlightRequest
 from monitoring.uss_qualifier.common_data_definitions import Severity
 
-class Result(ImplicitDict):
-    ''' A class to hold the expected output from data processing and the test interface of the USS under test '''
-    processing_outcome: str
-        
 class KnownIssueFields(ImplicitDict):
     ''' A class to hold a message that the test executor can provide to the USS in cases when the USS provides a response that is not same as the expected result. '''    
     test_code: str
@@ -30,23 +26,18 @@ class KnownIssueFields(ImplicitDict):
 class KnownResponses(ImplicitDict):
     ''' A class to evaluate results / response to an injection of test flight data (TestFlightRequest). This data structure holds information about the "result" response provided by the test interface of the USS under test and details of how long the executor should wait for a response. Per the SCD Testing API an acceptable result should be one of "Planned", "Rejected", "ConflictWithFlight" or "Failed". In the case where the USS under test provides a result that is not the same as expected result then the test driver can provide a message detailing why the expected result was populated the way it was. This should help the USS under test debug specific parts of their internal systems. '''
     acceptable_results: List[str] 
-    incorrect_result_details: Optional[Dict[str, KnownIssueFields]]
-    response_wait_time_secs: int 
-    response_timeout_message: KnownIssueFields
+    incorrect_result_details: Dict[str, KnownIssueFields]
+    
 
 class InjectionTarget(ImplicitDict):
     ''' A class to hold sequential assignment of the USS as per the SCD test API '''
-    uss_sequence_number: int
+    uss_role: str
 
 class FlightInjectionAttempt(ImplicitDict):
     ''' A class to hold details of the test injection, the injection target and the expected result of processing the flight request '''
     test_injection: InjectFlightRequest
     known_responses: KnownResponses
     injection_target: InjectionTarget
-
-class TestDetails(ImplicitDict):
-    ''' A class to hold details of different test structures '''
-    name: str # e.g. a Nominal test or Flight Authorisation test
 
 class AutomatedTest(ImplicitDict):
     ''' A class to hold injection attempts and test definitions '''
