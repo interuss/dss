3036fb481ac15c7acd27ba15c007735df7e0ec46
Pratibha <pratibhag@google.com>
2022-02-09 17:51:05 +1100

    RID down migrate file updated

--- test/migrations/migrate_db.sh
+++ test/migrations/migrate_db.sh
@@ -5,10 +5,18 @@ set -eo pipefail
 db_name=$1
 version=$2
 crdb_name=${3:-"dss-crdb-for-migration-testing"}
-network=${4:-"dss_sandbox_default"}
+
+if [[ -n "$4" ]]
+then
+  network_flag="--network $4"
+else
+  network_flag=""
+fi
+
+echo "network flag: ${network_flag}"
 
 echo "Migrating ${db_name} database to version ${version}"
-echo "crdb server: ${crdb_name} dss network ${network}"
+echo "crdb server: ${crdb_name} dss network ${network_flag}"
 
 echo " -------------- BOOTSTRAP ----------------- "
 echo "Building db-manager container for testing"
@@ -18,7 +26,7 @@ echo " ---------------- MIGRATE DATABASE -------------------- "
 echo "Migrating ${db_name} database to version ${version}"
 docker run --rm --name migration-testing-db-manager \
   --link "${crdb_name}":crdb \
-  --network "${network}" \
+  $network_flag \
   -v "$(pwd)/build/deploy/db_schemas/${db_name}:/db-schemas/${db_name}" \
   local-db-manager \
   --schemas_dir db-schemas/"${db_name}" \
