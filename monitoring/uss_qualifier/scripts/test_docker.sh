e508ebc60ca30f0fce1e44912bed8796a8024bbc
Michael Barroco <michael@orbitalize.com>
2022-02-19 16:45:36 +0100

    [ci] Clear image cache and print logs in case of failure

--- monitoring/uss_qualifier/scripts/test_docker.sh
+++ monitoring/uss_qualifier/scripts/test_docker.sh
@@ -10,6 +10,8 @@ echo "Ensure the environment is clean"
 echo "============="
 docker-compose -f docker-compose_qualifier_mocks.yaml -p $project_name down
 
+echo "Rebuild images without cache"
+docker-compose -f docker-compose_qualifier_mocks.yaml -p $project_name build --no-cache
 
 echo "Start mocks"
 echo "============="
@@ -25,6 +27,8 @@ for service_name in "${services[@]}"; do
     max_retry=3
     until [ "$(docker inspect -f \{\{.State.Health.Status\}\} "${container_name}")" == "healthy" ]; do
         if [ "$retry" -gt "$max_retry" ]; then
+            echo "$container_name logs:"
+            docker logs "$container_name"
             echo "$container_name didn't properly start. Exit." && exit 1
         fi
         sleep 1
