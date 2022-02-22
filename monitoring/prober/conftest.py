22c89bf416628d88a77b849c06bfa81664bb0de3
Pratibha <40968555+pratibhagupta2109@users.noreply.github.com>
2022-02-23 06:51:01 +1100

    [scd] Update SCD API reference to 1.0.0 (#712)
    * Updated SCD API reference to 1.0.0
    
    * Update SCD API submodule to target 1.0.0
    
    * submodule version referred to 1.0.0
    
    * Version updated for reference to uastech
    
    * uss-qualifier retry increased for testing
    
    * uastech reverted back to c062a4a1d5e8e1f682b6e0763bc29e9bc9a4da9d
    
    * make protos run
    
    * Retry reset to 3

--- monitoring/prober/conftest.py
+++ monitoring/prober/conftest.py
@@ -70,7 +70,7 @@ def pytest_addoption(parser):
   parser.addoption(
     '--scd-api-version',
     help='SCD API version to target',
-    choices=[scd.API_0_3_5, scd.API_0_3_17],
+    choices=[scd.API_0_3_5, scd.API_0_3_17, scd.API_1_0_0],
     default=scd.API_0_3_5,
     dest='scd_api_version')
 
