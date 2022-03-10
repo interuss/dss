6d0a2d5e9dcb57de710992671a6d33c2944380c3
Michael Barroco <michael@orbitalize.com>
2022-03-10 08:21:11 +0100

    [dss] Fix issue #732 (#734)

--- pkg/cockroach/cockroach.go
+++ pkg/cockroach/cockroach.go
@@ -3,6 +3,9 @@ package cockroach
 import (
 	"context"
 	"fmt"
+	"sort"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/coreos/go-semver/semver"
@@ -45,13 +48,106 @@ type DB struct {
 	Pool *pgxpool.Pool
 }
 
+func parseIntOrDefault(port string, defaultPort int64) int64 {
+	p, err := strconv.ParseInt(port, 10, 16)
+	if err != nil {
+		p = defaultPort
+	}
+	return p
+}
+
+// connectParametersFromMap constructs a ConnectParameters instance from m.
+func connectParametersFromMap(m map[string]string) ConnectParameters {
+	return ConnectParameters{
+		ApplicationName: m["application_name"],
+		DBName:          m["db_name"],
+		Host:            m["host"],
+		Port:            int(parseIntOrDefault(m["port"], 0)),
+		Credentials: Credentials{
+			Username: m["user"],
+		},
+		SSL: SSL{
+			Mode: m["ssl_mode"],
+			Dir:  m["ssl_dir"],
+		},
+		MaxOpenConns:       int(parseIntOrDefault(m["max_open_conns"], 4)),
+		MaxConnIdleSeconds: int(parseIntOrDefault(m["max_conn_idle_secs"], 40)),
+	}
+}
+
+// formatDSN constructs a DSN string from a key value map.
+func formatDSN(dsnMap map[string]string) string {
+	d := make([]string, 0)
+	for key, value := range dsnMap {
+		if value != "" {
+			d = append(d, fmt.Sprintf("%s=%s", key, value))
+		}
+	}
+	sort.Strings(d)
+	return strings.Join(d, " ")
+}
+
+// BuildURI returns a URI built from p.
+func (cp ConnectParameters) BuildDSN() (string, error) {
+	dsnMap := make(map[string]string)
+
+	u := cp.Credentials.Username
+	if u == "" {
+		return "", stacktrace.NewError("Missing crdb user")
+	}
+	dsnMap["user"] = u
+
+	h := cp.Host
+	if h == "" {
+		return "", stacktrace.NewError("Missing crdb hostname")
+	}
+	dsnMap["host"] = h
+
+	port := cp.Port
+	if port == 0 {
+		return "", stacktrace.NewError("Missing crdb port")
+	}
+	dsnMap["port"] = fmt.Sprintf("%d", port)
+
+	an := cp.ApplicationName
+	if an == "" {
+		an = "dss"
+	}
+	dsnMap["application_name"] = an
+
+	dsnMap["dbname"] = cp.DBName
+
+	sslMode := cp.SSL.Mode
+	if sslMode == "" {
+		return "", stacktrace.NewError("Missing crdb ssl_mode")
+	}
+	dsnMap["sslmode"] = sslMode
+
+	dsnMap["pool_max_conns"] = fmt.Sprintf("%d", cp.MaxOpenConns)
+
+	if sslMode == "disable" {
+		return formatDSN(dsnMap), nil
+	}
+
+	dir := cp.SSL.Dir
+	if dir == "" {
+		return "", stacktrace.NewError("Missing crdb ssl_dir")
+	}
+	dsnMap["sslrootcert"] = fmt.Sprintf("%s/ca.crt", dir)
+	dsnMap["sslcert"] = fmt.Sprintf("%s/client.%s.crt", dir, u)
+	dsnMap["sslkey"] = fmt.Sprintf("%s/client.%s.key", dir, u)
+
+	return formatDSN(dsnMap), nil
+}
+
 // Dial returns a DB instance connected to a cockroach instance available at
 // "uri".
 // https://www.cockroachlabs.com/docs/stable/connection-parameters.html
 func Dial(ctx context.Context, connParams ConnectParameters) (*DB, error) {
-	dsn := fmt.Sprintf("user=%s host=%s port=%d dbname=%s sslmode=%s pool_max_conns=%d",
-		connParams.Credentials.Username, connParams.Host, connParams.Port, connParams.DBName, connParams.SSL.Mode,
-		int32(connParams.MaxOpenConns))
+	dsn, err := connParams.BuildDSN()
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "Failed to create connection config for pgx")
+	}
 
 	config, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
