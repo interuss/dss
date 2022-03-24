b68856ba4bfa70643dd295dcdb465fb4367a6673
Pratibha <40968555+pratibhagupta2109@users.noreply.github.com>
2022-03-24 15:18:56 +1100

    [uss_qualifier/webapp] OpenAPI definition for uss_qualifier webapp interface: Initial check-in (#739)
    * qualifier readme links updated
    
    * Initial check-in for uss-qualifier rest api config file.
    
    * hardcode user_id
    
    * Remove untracked changes
    
    * comments incorporated

--- monitoring/uss_qualifier/webapp/forms.py
+++ monitoring/uss_qualifier/webapp/forms.py
@@ -1,3 +1,4 @@
+import flask
 import json
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, widgets, SelectMultipleField
@@ -29,3 +30,38 @@ class TestsExecuteForm(FlaskForm):
         if len(form.flight_records.data) < len(rid_config['injection_targets']):
             raise ValidationError(
                 'Not enough flight states files provided for each injection_targets.')
+
+class TestRunsForm(FlaskForm):
+    class Meta:
+        csrf = False
+    flight_records = StringField('Flight Records', validators=[DataRequired()])
+    auth_spec = StringField('Auth Spec', validators=[DataRequired()])
+    user_config = TextAreaField('User Config', validators=[DataRequired()])
+
+    def validate_user_config(form, field):
+        user_config = json.loads(field.data)
+        expected_keys = {'injection_targets', 'observers'}
+        if not user_config.get('rid'):
+            message = f'`rid` field missing in config object'
+            raise ValidationError(message)
+        rid_config = user_config['rid']
+        if not expected_keys.issubset(set(rid_config)):
+            message = f'{rid_config} missing fields in config object {expected_keys - set(rid_config)}'
+            raise ValidationError(message)
+        if len(form.flight_records.data) < len(rid_config['injection_targets']):
+            raise ValidationError(
+                'Not enough flight states files provided for each injection_targets.')
+
+
+def json_abort(status_code, message, details=None):
+    data = {
+        'error': {
+            'code': status_code,
+            'message': message
+        }
+    }
+    if details:
+        data['error']['details'] = details
+    response = flask.jsonify(data)
+    response.status_code = status_code
+    flask.abort(response)
