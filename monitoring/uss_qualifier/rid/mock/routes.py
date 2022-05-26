660b835ae3e256f87eaaff5c4001667de2190b21
Michael Barroco <michael@orbitalize.com>
2022-05-26 23:59:56 +0200

    Fix path and second pass format

--- monitoring/uss_qualifier/rid/mock/routes.py
+++ monitoring/uss_qualifier/rid/mock/routes.py
@@ -1,9 +1,9 @@
 from . import webapp
 
 
-@webapp.route('/status')
+@webapp.route("/status")
 def status():
-  return 'RID system mock for rid_qualifier is Ok', 200
+    return "RID system mock for rid_qualifier is Ok", 200
 
 
 from . import routes_injection
--- monitoring/uss_qualifier/scd/simulator/test_definitions/builder.py
+++ monitoring/uss_qualifier/scd/simulator/test_definitions/builder.py
@@ -3,12 +3,19 @@ import os
 from typing import List
 
 from monitoring.monitorlib.locality import Locality
-from monitoring.uss_qualifier.scd.data_interfaces import RequiredUSSCapabilities, AutomatedTest, TestStep
+from monitoring.uss_qualifier.scd.data_interfaces import (
+    RequiredUSSCapabilities,
+    AutomatedTest,
+    TestStep,
+)
 
 
-TEST_DEFINITIONS_BASEDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../', 'test_definitions'))
+TEST_DEFINITIONS_BASEDIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../../", "test_definitions")
+)
 
-class AutomatedTestBuilder():
+
+class AutomatedTestBuilder:
     name: str
     group: str
     locale: Locality
@@ -22,20 +29,23 @@ class AutomatedTestBuilder():
 
     def build(self) -> AutomatedTest:
         return AutomatedTest(
-            name=self.name,
-            uss_capabilities=self.uss_capabilities,
-            steps=self.steps
+            name=self.name, uss_capabilities=self.uss_capabilities, steps=self.steps
         )
 
     def save(self):
         output = self.build()
         output_path = self.get_output_path()
         print(f"Saving test '{self.name}' ({self.group}) to {output_path}")
-        with open(output_path, 'w') as f:
+        with open(output_path, "w") as f:
             f.write(json.dumps(output))
 
     def get_filename(self):
-        return self.name.replace(' ', '-').lower() + '.json'
+        return self.name.replace(" ", "-").lower() + ".json"
 
     def get_output_path(self):
-        return os.path.join(TEST_DEFINITIONS_BASEDIR, self.locale.upper(), self.group, self.get_filename())
+        return os.path.join(
+            TEST_DEFINITIONS_BASEDIR,
+            self.locale.upper(),
+            self.group,
+            self.get_filename(),
+        )
