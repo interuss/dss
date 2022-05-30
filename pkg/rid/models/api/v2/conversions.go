17f53f1feb2189279974cd2db9b59be53568ff3d
Benjamin Pelletier <pelletierb@wing.com>
2022-05-30 10:00:35 -0700

    Add RID v2 endpoints

---
+++ pkg/rid/models/api/v2/conversions.go
@@ -0,0 +1,228 @@
+package apiv2
+
+import (
+	"strings"
+	"time"
+
+	ridpb "github.com/interuss/dss/pkg/api/v2/ridpbv2"
+	dssmodels "github.com/interuss/dss/pkg/models"
+	ridmodels "github.com/interuss/dss/pkg/rid/models"
+	"github.com/interuss/stacktrace"
+	tspb "google.golang.org/protobuf/types/known/timestamppb"
+)
+
+// === RID -> Business ===
+
+// FromTime converts proto to standard golang Time
+func FromTime(t *ridpb.Time) (*time.Time, error) {
+	if t == nil {
+		return nil, nil
+	}
+	format := t.GetFormat()
+	if format != "RFC3339" {
+		return nil, stacktrace.NewError("Invalid time format '" + format + "'; expected 'RFC3339'")
+	}
+	value := t.GetValue()
+	if value == nil {
+		return nil, stacktrace.NewError("Time structure specified, but `value` was missing")
+	}
+	err := value.CheckValid()
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "Error converting time from proto")
+	}
+	ts := value.AsTime()
+	return &ts, nil
+}
+
+// FromAltitude converts proto to float
+func FromAltitude(alt *ridpb.Altitude) (*float32, error) {
+	if alt == nil {
+		return nil, nil
+	}
+	ref := alt.GetReference()
+	if ref != "WGS84" {
+		return nil, stacktrace.NewError("Invalid altitude reference '" + ref + "'; expected 'WGS84'")
+	}
+	units := alt.GetUnits()
+	if units != "M" {
+		return nil, stacktrace.NewError("Invalid units '" + units + "'; expected 'M'")
+	}
+	value := float32(alt.GetValue())
+	return &value, nil
+}
+
+// FromVolume4D converts proto to business object
+func FromVolume4D(vol4 *ridpb.Volume4D) (*dssmodels.Volume4D, error) {
+	vol3, err := FromVolume3D(vol4.GetVolume())
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "Error parsing spatial volume of Volume4D")
+	}
+
+	result := &dssmodels.Volume4D{
+		SpatialVolume: vol3,
+	}
+
+	result.StartTime, err = FromTime(vol4.GetTimeStart())
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "Error parsing start time of Volume4D")
+	}
+	result.EndTime, err = FromTime(vol4.GetTimeEnd())
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "Error parsing end time of Volume4D")
+	}
+
+	return result, nil
+}
+
+// FromVolume3D converts proto to business object
+func FromVolume3D(vol3 *ridpb.Volume3D) (*dssmodels.Volume3D, error) {
+	altitudeLo, err := FromAltitude(vol3.GetAltitudeLower())
+	if err != nil {
+		stacktrace.Propagate(err, "Error parsing lower altitude of Volume3D")
+	}
+	altitudeHi, err := FromAltitude(vol3.GetAltitudeUpper())
+	if err != nil {
+		stacktrace.Propagate(err, "Error parsing upper altitude of Volume3D")
+	}
+
+	polygon := vol3.GetOutlinePolygon()
+	if polygon != nil {
+		footprint := FromPolygon(polygon)
+
+		result := &dssmodels.Volume3D{
+			Footprint:  footprint,
+			AltitudeLo: altitudeLo,
+			AltitudeHi: altitudeHi,
+		}
+
+		return result, nil
+	}
+
+	circle := vol3.GetOutlineCircle()
+	if circle != nil {
+		footprint, err := FromCircle(circle)
+		if err != nil {
+			return nil, stacktrace.Propagate(err, "Error parsing outline_circle for Volume3D")
+		}
+
+		result := &dssmodels.Volume3D{
+			Footprint:  footprint,
+			AltitudeLo: altitudeLo,
+			AltitudeHi: altitudeHi,
+		}
+
+		return result, nil
+	}
+
+	return nil, stacktrace.NewError("Neither outline_polygon nor outline_circle were specified in volume")
+}
+
+// FromPolygon converts proto to business object
+func FromPolygon(polygon *ridpb.Polygon) *dssmodels.GeoPolygon {
+	result := &dssmodels.GeoPolygon{}
+
+	for _, ltlng := range polygon.Vertices {
+		result.Vertices = append(result.Vertices, FromLatLngPoint(ltlng))
+	}
+
+	return result
+}
+
+// FromCircle converts proto to business object
+func FromCircle(circle *ridpb.Circle) (*dssmodels.GeoCircle, error) {
+	center := circle.GetCenter()
+	if center == nil {
+		return nil, stacktrace.NewError("Missing `center` from circle")
+	}
+	radius := circle.GetRadius()
+	if radius == nil {
+		return nil, stacktrace.NewError("Missing `radius` from circle")
+	}
+	if radius.GetUnits() != "M" {
+		return nil, stacktrace.NewError("Only circle radius units of 'M' are acceptable for UTM")
+	}
+	result := &dssmodels.GeoCircle{
+		Center:      *FromLatLngPoint(center),
+		RadiusMeter: radius.GetValue(),
+	}
+	return result, nil
+}
+
+// FromLatLngPoint converts proto to business object
+func FromLatLngPoint(pt *ridpb.LatLngPoint) *dssmodels.LatLngPoint {
+	return &dssmodels.LatLngPoint{
+		Lat: pt.Lat,
+		Lng: pt.Lng,
+	}
+}
+
+// === Business -> RID ===
+
+// ToTime converts standard golang Time to proto
+func ToTime(t *time.Time) *ridpb.Time {
+	if t == nil {
+		return nil
+	}
+
+	result := &ridpb.Time{
+		Format: "RFC3339",
+		Value:  tspb.New(*t),
+	}
+
+	return result
+}
+
+// ToLatLngPoint converts latlngpoint business object to proto
+func ToLatLngPoint(pt *dssmodels.LatLngPoint) *ridpb.LatLngPoint {
+	result := &ridpb.LatLngPoint{
+		Lat: pt.Lat,
+		Lng: pt.Lng,
+	}
+
+	return result
+}
+
+// IdentificationServiceAreaToProto converts an IdentificationServiceArea
+// business object to v2 proto for API consumption.
+func ToIdentificationServiceArea(i *ridmodels.IdentificationServiceArea) *ridpb.IdentificationServiceArea {
+	result := &ridpb.IdentificationServiceArea{
+		Id:         i.ID.String(),
+		Owner:      i.Owner.String(),
+		UssBaseUrl: strings.TrimSuffix(strings.TrimSuffix(i.URL, "/v1/uss/flights"), "/uss/flights"),
+		Version:    i.Version.String(),
+		TimeStart:  ToTime(i.StartTime),
+		TimeEnd:    ToTime(i.EndTime),
+	}
+
+	return result
+}
+
+// ToSubscriberToNotify converts a subscription to a SubscriberToNotify proto
+// for API consumption.
+func ToSubscriberToNotify(s *ridmodels.Subscription) *ridpb.SubscriberToNotify {
+	return &ridpb.SubscriberToNotify{
+		Url: s.URL,
+		Subscriptions: []*ridpb.SubscriptionState{
+			{
+				NotificationIndex: int32(s.NotificationIndex),
+				SubscriptionId:    s.ID.String(),
+			},
+		},
+	}
+}
+
+// ToSubscription converts a subscription business object to a Subscription
+// proto for API consumption.
+func ToSubscription(s *ridmodels.Subscription) *ridpb.Subscription {
+	result := &ridpb.Subscription{
+		Id:                s.ID.String(),
+		Owner:             s.Owner.String(),
+		UssBaseUrl:        strings.TrimSuffix(strings.TrimSuffix(s.URL, "/v1/uss/identification_service_areas"), "/uss/identification_service_areas"),
+		NotificationIndex: int32(s.NotificationIndex),
+		Version:           s.Version.String(),
+		TimeStart:         ToTime(s.StartTime),
+		TimeEnd:           ToTime(s.EndTime),
+	}
+
+	return result
+}
---
+++ pkg/rid/server/v2/isa_handler.go
@@ -0,0 +1,267 @@
+package server
+
+import (
+	"context"
+	"time"
+
+	ridpb "github.com/interuss/dss/pkg/api/v2/ridpbv2"
+	"github.com/interuss/dss/pkg/auth"
+	dsserr "github.com/interuss/dss/pkg/errors"
+	"github.com/interuss/dss/pkg/geo"
+	geoerr "github.com/interuss/dss/pkg/geo"
+	dssmodels "github.com/interuss/dss/pkg/models"
+	ridmodels "github.com/interuss/dss/pkg/rid/models"
+	apiv2 "github.com/interuss/dss/pkg/rid/models/api/v2"
+	"github.com/interuss/stacktrace"
+	"github.com/pkg/errors"
+)
+
+// GetIdentificationServiceArea returns a single ISA for a given ID.
+func (s *Server) GetIdentificationServiceArea(
+	ctx context.Context, req *ridpb.GetIdentificationServiceAreaRequest) (
+	*ridpb.GetIdentificationServiceAreaResponse, error) {
+
+	id, err := dssmodels.IDFromString(req.Id)
+	if err != nil {
+		return nil, stacktrace.NewErrorWithCode(dsserr.BadRequest, "Invalid ID format")
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, s.Timeout)
+	defer cancel()
+	isa, err := s.App.GetISA(ctx, id)
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "Could not get ISA from application layer")
+	}
+	if isa == nil {
+		return nil, stacktrace.NewErrorWithCode(dsserr.NotFound, "ISA %s not found", req.GetId())
+	}
+	return &ridpb.GetIdentificationServiceAreaResponse{
+		ServiceArea: apiv2.ToIdentificationServiceArea(isa),
+	}, nil
+}
+
+// CreateIdentificationServiceArea creates an ISA
+func (s *Server) CreateIdentificationServiceArea(
+	ctx context.Context, req *ridpb.CreateIdentificationServiceAreaRequest) (
+	*ridpb.PutIdentificationServiceAreaResponse, error) {
+
+	params := req.GetParams()
+	ctx, cancel := context.WithTimeout(ctx, s.Timeout)
+	defer cancel()
+
+	owner, ok := auth.OwnerFromContext(ctx)
+	if !ok {
+		return nil, stacktrace.NewErrorWithCode(dsserr.PermissionDenied, "Missing owner from context")
+	}
+	if params == nil {
+		return nil, stacktrace.NewErrorWithCode(dsserr.BadRequest, "Params not set")
+	}
+	// TODO: put the validation logic in the models layer
+	if params.UssBaseUrl == "" {
+		return nil, stacktrace.NewErrorWithCode(dsserr.BadRequest, "Missing required USS base URL")
+	}
+	if params.Extents == nil {
+		return nil, stacktrace.NewErrorWithCode(dsserr.BadRequest, "Missing required extents")
+	}
+	extents, err := apiv2.FromVolume4D(params.Extents)
+	if err != nil {
+		return nil, stacktrace.NewErrorWithCode(dsserr.BadRequest, "Error parsing Volume4D")
+	}
+	id, err := dssmodels.IDFromString(req.Id)
+	if err != nil {
+		return nil, stacktrace.NewErrorWithCode(dsserr.BadRequest, "Invalid ID format")
+	}
+
+	if !s.EnableHTTP {
+		err = ridmodels.ValidateURL(params.GetUssBaseUrl())
+		if err != nil {
+			return nil, stacktrace.PropagateWithCode(err, dsserr.BadRequest, "Failed to validate base URL")
+		}
+	}
+
+	isa := &ridmodels.IdentificationServiceArea{
+		ID:     id,
+		URL:    params.GetUssBaseUrl() + "/uss/flights",
+		Owner:  owner,
+		Writer: s.Locality,
+	}
+
+	if err := isa.SetExtents(extents); err != nil {
+		return nil, stacktrace.PropagateWithCode(err, dsserr.BadRequest, "Invalid extents")
+	}
+
+	insertedISA, subscribers, err := s.App.InsertISA(ctx, isa)
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "Could not insert ISA")
+	}
+
+	pbISA := apiv2.ToIdentificationServiceArea(insertedISA)
+
+	pbSubscribers := []*ridpb.SubscriberToNotify{}
+	for _, subscriber := range subscribers {
+		pbSubscribers = append(pbSubscribers, apiv2.ToSubscriberToNotify(subscriber))
+	}
+
+	return &ridpb.PutIdentificationServiceAreaResponse{
+		ServiceArea: pbISA,
+		Subscribers: pbSubscribers,
+	}, nil
+}
+
+// UpdateIdentificationServiceArea updates an existing ISA.
+func (s *Server) UpdateIdentificationServiceArea(
+	ctx context.Context, req *ridpb.UpdateIdentificationServiceAreaRequest) (
+	*ridpb.PutIdentificationServiceAreaResponse, error) {
+
+	params := req.GetParams()
+
+	version, err := dssmodels.VersionFromString(req.GetVersion())
+	if err != nil {
+		return nil, stacktrace.PropagateWithCode(err, dsserr.BadRequest, "Invalid version")
+	}
+	ctx, cancel := context.WithTimeout(ctx, s.Timeout)
+	defer cancel()
+
+	owner, ok := auth.OwnerFromContext(ctx)
+	if !ok {
+		return nil, stacktrace.NewErrorWithCode(dsserr.PermissionDenied, "Missing owner from context")
+	}
+	// TODO: put the validation logic in the models layer
+	if params == nil {
+		return nil, stacktrace.NewErrorWithCode(dsserr.BadRequest, "Params not set")
+	}
+	if params.UssBaseUrl == "" {
+		return nil, stacktrace.NewErrorWithCode(dsserr.BadRequest, "Missing required USS base URL")
+	}
+	if params.Extents == nil {
+		return nil, stacktrace.NewErrorWithCode(dsserr.BadRequest, "Missing required extents")
+	}
+	extents, err := apiv2.FromVolume4D(params.Extents)
+	if err != nil {
+		return nil, stacktrace.NewErrorWithCode(dsserr.BadRequest, "Error parsing Volume4D")
+	}
+	id, err := dssmodels.IDFromString(req.Id)
+	if err != nil {
+		return nil, stacktrace.NewErrorWithCode(dsserr.BadRequest, "Invalid ID format")
+	}
+
+	isa := &ridmodels.IdentificationServiceArea{
+		ID:      dssmodels.ID(id),
+		URL:     params.UssBaseUrl + "/uss/flights",
+		Owner:   owner,
+		Version: version,
+		Writer:  s.Locality,
+	}
+
+	if err := isa.SetExtents(extents); err != nil {
+		return nil, stacktrace.PropagateWithCode(err, dsserr.BadRequest, "Invalid extents")
+	}
+
+	insertedISA, subscribers, err := s.App.UpdateISA(ctx, isa)
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "Could not update ISA")
+	}
+
+	pbISA := apiv2.ToIdentificationServiceArea(insertedISA)
+
+	pbSubscribers := []*ridpb.SubscriberToNotify{}
+	for _, subscriber := range subscribers {
+		pbSubscribers = append(pbSubscribers, apiv2.ToSubscriberToNotify(subscriber))
+	}
+
+	return &ridpb.PutIdentificationServiceAreaResponse{
+		ServiceArea: pbISA,
+		Subscribers: pbSubscribers,
+	}, nil
+}
+
+// DeleteIdentificationServiceArea deletes an existing ISA.
+func (s *Server) DeleteIdentificationServiceArea(
+	ctx context.Context, req *ridpb.DeleteIdentificationServiceAreaRequest) (
+	*ridpb.DeleteIdentificationServiceAreaResponse, error) {
+
+	owner, ok := auth.OwnerFromContext(ctx)
+	if !ok {
+		return nil, stacktrace.NewErrorWithCode(dsserr.PermissionDenied, "Missing owner from context")
+	}
+	version, err := dssmodels.VersionFromString(req.GetVersion())
+	if err != nil {
+		return nil, stacktrace.PropagateWithCode(err, dsserr.BadRequest, "Invalid version")
+	}
+	id, err := dssmodels.IDFromString(req.Id)
+	if err != nil {
+		return nil, stacktrace.NewErrorWithCode(dsserr.BadRequest, "Invalid ID format")
+	}
+	ctx, cancel := context.WithTimeout(ctx, s.Timeout)
+	defer cancel()
+	isa, subscribers, err := s.App.DeleteISA(ctx, id, owner, version)
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "Could not delete ISA")
+	}
+
+	p := apiv2.ToIdentificationServiceArea(isa)
+	sp := make([]*ridpb.SubscriberToNotify, len(subscribers))
+	for i := range subscribers {
+		sp[i] = apiv2.ToSubscriberToNotify(subscribers[i])
+	}
+
+	return &ridpb.DeleteIdentificationServiceAreaResponse{
+		ServiceArea: p,
+		Subscribers: sp,
+	}, nil
+}
+
+// SearchIdentificationServiceAreas queries for all ISAs in the bounds.
+func (s *Server) SearchIdentificationServiceAreas(
+	ctx context.Context, req *ridpb.SearchIdentificationServiceAreasRequest) (
+	*ridpb.SearchIdentificationServiceAreasResponse, error) {
+
+	cu, err := geo.AreaToCellIDs(req.GetArea())
+	if err != nil {
+		if errors.Is(err, geoerr.ErrAreaTooLarge) {
+			return nil, stacktrace.Propagate(err, "Invalid area")
+		}
+		return nil, stacktrace.PropagateWithCode(err, dsserr.BadRequest, "Invalid area")
+	}
+
+	var (
+		earliest *time.Time
+		latest   *time.Time
+	)
+
+	if et := req.GetEarliestTime(); et != nil {
+		ts := et.AsTime()
+		err := et.CheckValid()
+		if err == nil {
+			earliest = &ts
+		} else {
+			return nil, stacktrace.Propagate(err, "Unable to convert earliest timestamp to ptype")
+		}
+	}
+
+	if lt := req.GetLatestTime(); lt != nil {
+		ts := lt.AsTime()
+		err := lt.CheckValid()
+		if err == nil {
+			latest = &ts
+		} else {
+			return nil, stacktrace.Propagate(err, "Unable to convert latest timestamp to ptype")
+		}
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, s.Timeout)
+	defer cancel()
+	isas, err := s.App.SearchISAs(ctx, cu, earliest, latest)
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "Unable to search ISAs")
+	}
+
+	areas := make([]*ridpb.IdentificationServiceArea, len(isas))
+	for i := range isas {
+		areas[i] = apiv2.ToIdentificationServiceArea(isas[i])
+	}
+
+	return &ridpb.SearchIdentificationServiceAreasResponse{
+		ServiceAreas: areas,
+	}, nil
+}
