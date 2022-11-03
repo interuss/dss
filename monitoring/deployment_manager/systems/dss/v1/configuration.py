b15c0cb933340e57706749e8fcab9484563dcab0
Mickaël Misbach <mickael.misbach@orbitalize.com>
2022-11-03 18:34:55 +0100

    update doc

--- monitoring/deployment_manager/systems/dss/v1/configuration.py
+++ monitoring/deployment_manager/systems/dss/v1/configuration.py
@@ -4,9 +4,9 @@ from implicitdict import ImplicitDict
 class V1DSS(ImplicitDict):
     """Definition of a version 1 DSS instance.
 
-    A v1 DSS system is a DSS instance consisting of CRDB nodes, core-service
-    workers, and http-gateway translators, initially defined with Jsonnet in
-    build/deploy to be applied to a Kubernetes cluster with Tanka.
+    A v1 DSS system is a DSS instance consisting of CRDB nodes, and core-service
+    workers, initially defined with Jsonnet in build/deploy to be applied to a
+    Kubernetes cluster with Tanka.
     """
 
     namespace: str = 'default'
