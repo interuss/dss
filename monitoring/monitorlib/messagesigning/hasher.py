1e79681df03c426379fddc997b9da3d78c4b7a36
kpkeenan12 <kpkeenan2493@gmail.com>
2022-12-22 10:59:00 -0800

    review suggestion for type

--- monitoring/monitorlib/messagesigning/hasher.py
+++ monitoring/monitorlib/messagesigning/hasher.py
@@ -5,7 +5,6 @@ from loguru import logger
 
 def get_content_digest(payload):
     payload = payload if payload else bytes()
-    type_of_payload = str(type(payload))
-    if type_of_payload == "<class 'str'>":
+    if isinstance(payload, str):
         payload = payload.encode("utf-8")
     return base64.b64encode(hashlib.sha512(payload).digest()).decode("utf-8")
