8ab48f9364093c87e9c5cbf802d1fdc1a777cc7b
Michael Barroco <michael@orbitalize.com>
2024-01-03 16:32:38 +0100

    [ci] Update the CI to deploy the DSS using the helm chart

--- deploy/operations/ci/aws-1/kubernetes_admin_access.tf
+++ deploy/operations/ci/aws-1/kubernetes_admin_access.tf
@@ -1,38 +1,41 @@
-# This module is expected to be applied by the Github CI user. By default, only the user has permission to
-# connect to the cluster. This file gathers resources to grant access to AWS administrators.
+// This module is expected to be applied by the Github CI user. By default, only the user who created the cluster
+// has permission to connect to the cluster. This file gathers resources to grant access to AWS administrators.
 
-resource "kubernetes_config_map_v1_data" "aws-auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
+resource "local_file" "aws-auth-config-map" {
+  content = yamlencode({
+    apiVersion = "v1"
+    kind = "ConfigMap"
+    metadata = {
+      name      = "aws-auth"
+      namespace = "kube-system"
+    }
+    data = {
+      mapRoles = yamlencode([
+        {
+          groups   = [
+            "system:bootstrappers",
+            "system:nodes"
+          ]
+          rolearn  = module.terraform-aws-dss.iam_role_node_group_arn
+          username = "system:node:{{EC2PrivateDNSName}}"
+        },
+        {
+          groups   = [
+            "system:masters"
+          ]
+          rolearn  = var.aws_iam_administrator_role
+          username = "interuss-aws-administrator"
+        },
+        {
+          groups   = [
+            "system:masters"
+          ]
+          rolearn  = var.aws_iam_ci_role
+          username = "interuss-ci"
+        }
+      ])
+    }
+  })
 
-  force = true # EKS provisions this file by default.
-
-  data = {
-    mapRoles = yamlencode([
-      {
-        groups   = [
-          "system:bootstrappers",
-          "system:nodes"
-        ]
-        rolearn  = module.terraform-aws-kubernetes.iam_role_node_group_arn
-        username = "system:node:{{EC2PrivateDNSName}}"
-      },
-      {
-        groups   = [
-          "system:masters"
-        ]
-        rolearn  = var.aws_iam_administrator_role
-        username = "interuss-aws-administrator"
-      },
-      {
-        groups   = [
-          "system:masters"
-        ]
-        rolearn  = var.aws_iam_ci_role
-        username = "interuss-ci"
-      }
-    ])
-  }
+  filename   = "${module.terraform-aws-dss.workspace_location}/aws_auth_config_map.yml"
 }
--- deploy/operations/ci/aws-1/output.tf
+++ deploy/operations/ci/aws-1/output.tf
@@ -1,4 +1,11 @@
 output "generated_files_location" {
-  value = module.terraform-commons-dss.generated_files_location
+  value = module.terraform-aws-dss.generated_files_location
 }
 
+output "workspace_location" {
+  value = module.terraform-aws-dss.workspace_location
+}
+
+output "cluster_context" {
+  value = module.terraform-aws-dss.cluster_context
+}
