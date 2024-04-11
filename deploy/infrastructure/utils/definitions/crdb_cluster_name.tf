8397e5395dd0e80b3e7241d7ecb55cd196f4023c
Michael Barroco <michael@orbitalize.com>
2024-04-11 23:31:57 +0200

    [terraform] add configuration variable crdb_cluster_name
    [terraform] add configuration variable crdb_cluster_name

---
+++ deploy/infrastructure/utils/definitions/crdb_cluster_name.tf
@@ -0,0 +1,14 @@
+variable "crdb_cluster_name" {
+  type        = string
+  description = <<-EOT
+    A string that specifies a CRDB cluster name. This is used together to ensure that all newly created
+    nodes join the intended cluster when you are running multiple clusters.
+    The CRDB cluster is automatically given a randomly-generated name. If desired, change the cluster's name.
+    The CRDB cluster name must be 6-20 characters in length, and can include lowercase letters, numbers,
+    and dashes (but no leading or trailing dashes). A cluster's name cannot be edited after it is created.
+
+    At the moment, this variable is only used for helm charts deployments.
+
+    Example: interuss_us_production
+  EOT
+}
