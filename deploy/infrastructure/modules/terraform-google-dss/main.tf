16a5cea045b4b6f84d5a86cb553ac6c4407f27f3
Michael Barroco <michael@orbitalize.com>
2024-04-11 21:40:50 +0200

    [terraform] pass missing crdb_external_nodes

--- deploy/infrastructure/modules/terraform-google-dss/main.tf
+++ deploy/infrastructure/modules/terraform-google-dss/main.tf
@@ -24,6 +24,7 @@ module "terraform-commons-dss" {
   authorization                  = var.authorization
   crdb_locality                  = var.crdb_locality
   image_pull_secret              = var.image_pull_secret
+  crdb_external_nodes            = var.crdb_external_nodes
   kubernetes_api_endpoint        = module.terraform-google-kubernetes.kubernetes_api_endpoint
   crdb_internal_nodes            = module.terraform-google-kubernetes.crdb_nodes
   ip_gateway                     = module.terraform-google-kubernetes.ip_gateway
