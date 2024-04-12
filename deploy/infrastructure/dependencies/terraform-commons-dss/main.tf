0c9fbf24636c1bb8802e66adc242e23d7efefa39
Michael Barroco <michael@orbitalize.com>
2024-04-12 14:57:43 +0200

    clean spaces

--- deploy/infrastructure/dependencies/terraform-commons-dss/main.tf
+++ deploy/infrastructure/dependencies/terraform-commons-dss/main.tf
@@ -3,6 +3,3 @@ locals {
   # Replace ':' and '/' characters from folder name by underscores. Those characters are used by AWS for contexts.
   workspace_location = abspath("${path.module}/../../../../build/workspace/${local.workspace_folder}")
 }
-
-
-
