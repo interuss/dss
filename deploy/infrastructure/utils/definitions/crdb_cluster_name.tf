260a6d6ffe53415cac37d4638a831e30970c236d
Michael Barroco <michael@orbitalize.com>
2024-04-12 16:51:25 +0200

    Update text

--- deploy/infrastructure/utils/definitions/crdb_cluster_name.tf
+++ deploy/infrastructure/utils/definitions/crdb_cluster_name.tf
@@ -3,7 +3,7 @@ variable "crdb_cluster_name" {
   description = <<-EOT
     A string that specifies a CRDB cluster name. This is used together to ensure that all newly created
     nodes join the intended cluster when you are running multiple clusters.
-    The CRDB cluster is automatically given a randomly-generated name. If desired, change the cluster's name.
+    The CRDB cluster is automatically given a randomly-generated name if an empty string is provided.
     The CRDB cluster name must be 6-20 characters in length, and can include lowercase letters, numbers,
     and dashes (but no leading or trailing dashes). A cluster's name cannot be edited after it is created.
 
