0b6c35e511a964a3ea4ac5a161d796ea99efe75a
Michael Barroco <michael@orbitalize.com>
2024-05-15 14:00:21 +0200

    [terraform] remove latest image setting

--- deploy/infrastructure/modules/terraform-google-dss/variables.gen.tf
+++ deploy/infrastructure/modules/terraform-google-dss/variables.gen.tf
@@ -110,15 +110,20 @@ variable "image" {
   description = <<EOT
   URL of the DSS docker image.
 
-
-  `latest` can be used to use the latest official interuss docker image.
   Official public images are available on Docker Hub: https://hub.docker.com/r/interuss/dss/tags
   See [/build/README.md](../../../../build/README.md#docker-images) Docker images section to learn
   how to build and publish your own image.
 
-  Example: `latest` or `docker.io/interuss/dss:v0.6.0`
+  Example: `docker.io/interuss/dss:latest` or `docker.io/interuss/dss:v0.14.0`
   EOT
+
+  validation {
+    condition = var.image != "latest"
+    error_message = "latest value is not supported anymore. Use `docker.io/interuss/dss:latest` for similar behavior."
+  }
+
 }
+
 
 variable "image_pull_secret" {
   type        = string
