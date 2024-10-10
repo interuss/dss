dc050c2b5577fd611efe42c86cfcc428d1268052
Michael Barroco <michael@orbitalize.com>
2024-10-10 20:46:36 +0200

    [terraform-aws] Pin aws-load-balancer-controller to v2.9 (helm chart: 1.9) (#1128)

--- deploy/infrastructure/dependencies/terraform-aws-kubernetes/network_lb.tf
+++ deploy/infrastructure/dependencies/terraform-aws-kubernetes/network_lb.tf
@@ -4,6 +4,7 @@ resource "helm_release" "aws-load-balancer-controller" {
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
   name       = "aws-load-balancer-controller"
+  version    = "1.9.0"
 
   namespace = "kube-system"
 
