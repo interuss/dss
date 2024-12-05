6ba104fc78e7784f4531a1a23de04e6490b9e50e
Michael Barroco <michael@orbitalize.com>
2024-12-05 11:29:16 +0100

    Flatten yugabyte scd migration

--- build/db_schemas/yugabyte/scd/upto-v1.0.0-crdb_v3.2.0.sql
+++ build/db_schemas/yugabyte/scd/upto-v1.0.0-crdb_v3.2.0.sql
@@ -1,3 +1,5 @@
+-- This migration is equivalent to scd v3.2.0 schema for CockroachDB.
+
 CREATE TABLE IF NOT EXISTS scd_subscriptions (
   id UUID PRIMARY KEY,
   owner TEXT NOT NULL,
@@ -10,21 +12,16 @@ CREATE TABLE IF NOT EXISTS scd_subscriptions (
   starts_at TIMESTAMPTZ,
   ends_at TIMESTAMPTZ,
   updated_at TIMESTAMPTZ NOT NULL,
+  cells BIGINT[],
   CHECK (starts_at IS NULL OR ends_at IS NULL OR starts_at < ends_at),
   CHECK (notify_for_operations OR notify_for_constraints)
 );
 CREATE INDEX ss_owner_idx ON scd_subscriptions (owner);
 CREATE INDEX ss_starts_at_idx ON scd_subscriptions (starts_at);
 CREATE INDEX ss_ends_at_idx ON scd_subscriptions (ends_at);
+CREATE INDEX ss_cells_idx ON scd_subscriptions USING ybgin (cells);
 
-CREATE TABLE IF NOT EXISTS scd_cells_subscriptions (
-  cell_id BIGINT NOT NULL,
-  cell_level INT CHECK (cell_level BETWEEN 0 and 30),
-  subscription_id UUID NOT NULL REFERENCES scd_subscriptions (id) ON DELETE CASCADE,
-  PRIMARY KEY (cell_id, subscription_id)
-);
-CREATE INDEX scs_cell_id_idx ON scd_cells_subscriptions (cell_id);
-CREATE INDEX scs_subscription_id_idx ON scd_cells_subscriptions (subscription_id);
+CREATE TYPE operational_intent_state AS ENUM ('Unknown', 'Accepted', 'Activated', 'Nonconforming', 'Contingent');
 
 CREATE TABLE IF NOT EXISTS scd_operations (
   id UUID PRIMARY KEY,
@@ -35,9 +32,19 @@ CREATE TABLE IF NOT EXISTS scd_operations (
   altitude_upper REAL,
   starts_at TIMESTAMPTZ,
   ends_at TIMESTAMPTZ,
-  subscription_id UUID NOT NULL REFERENCES scd_subscriptions(id) ON DELETE CASCADE,
+  subscription_id UUID REFERENCES scd_subscriptions(id) ON DELETE CASCADE,
   updated_at TIMESTAMPTZ NOT NULL,
-  CHECK (starts_at IS NULL OR ends_at IS NULL OR starts_at < ends_at)
+  state operational_intent_state NOT NULL DEFAULT 'Unknown',
+  cells BIGINT[],
+  uss_requested_ovn TEXT,
+  past_ovns TEXT[] NOT NULL DEFAULT ARRAY []::TEXT[],
+  CHECK (starts_at IS NULL OR ends_at IS NULL OR starts_at < ends_at),
+  CHECK (uss_requested_ovn != ''),
+  CHECK (
+      array_position(past_ovns, NULL) IS NULL AND
+      array_position(past_ovns, '') IS NULL AND
+      array_position(past_ovns, uss_requested_ovn) IS NULL
+      )
 );
 CREATE INDEX so_owner_idx ON scd_operations (owner);
 CREATE INDEX so_altitude_lower_idx ON scd_operations (altitude_lower);
@@ -46,15 +53,7 @@ CREATE INDEX so_starts_at_idx ON scd_operations (starts_at);
 CREATE INDEX so_ends_at_idx ON scd_operations (ends_at);
 CREATE INDEX so_updated_at_idx ON scd_operations (updated_at);
 CREATE INDEX so_subscription_id_idx ON scd_operations (subscription_id);
-
-CREATE TABLE IF NOT EXISTS scd_cells_operations (
-  cell_id BIGINT NOT NULL,
-  cell_level INT CHECK (cell_level BETWEEN 0 and 30),
-  operation_id UUID NOT NULL REFERENCES scd_operations (id) ON DELETE CASCADE,
-  PRIMARY KEY (cell_id, operation_id)
-);
-CREATE INDEX sco_cell_id_idx ON scd_cells_operations (cell_id);
-CREATE INDEX sco_operation_id_idx ON scd_cells_operations (operation_id);
+CREATE INDEX so_cells_idx ON scd_operations USING ybgin (cells);
 
 CREATE TABLE IF NOT EXISTS scd_constraints (
   id UUID PRIMARY KEY,
@@ -74,9 +73,16 @@ CREATE INDEX sc_starts_at_idx ON scd_constraints (starts_at);
 CREATE INDEX sc_ends_at_idx ON scd_constraints (ends_at);
 CREATE INDEX sc_cells_idx ON scd_constraints USING ybgin (cells);
 
+
+CREATE TABLE IF NOT EXISTS scd_uss_availability (
+  id TEXT PRIMARY KEY,
+  availability TEXT NOT NULL,
+  updated_at TIMESTAMPTZ NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS schema_versions (
-	onerow_enforcer bool PRIMARY KEY DEFAULT TRUE CHECK(onerow_enforcer),
-	schema_version TEXT NOT NULL
+  onerow_enforcer bool PRIMARY KEY DEFAULT TRUE CHECK(onerow_enforcer),
+  schema_version TEXT NOT NULL
 );
 
 INSERT INTO schema_versions (schema_version) VALUES ('v1.0.0');
