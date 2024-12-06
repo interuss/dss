743d428f3596b2a4d7d6fdf95c58f31af99bf53d
Michael Barroco <michael@orbitalize.com>
2024-12-06 21:31:32 +0100

    Fix down scd migration

--- build/db_schemas/yugabyte/scd/downfrom-v1.0.0-crdb_v3.2.0.sql
+++ build/db_schemas/yugabyte/scd/downfrom-v1.0.0-crdb_v3.2.0.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS schema_versions;
+DROP TABLE IF EXISTS scd_uss_availability;
 DROP TABLE IF EXISTS scd_constraints;
-DROP TABLE IF EXISTS scd_cells_operations;
 DROP TABLE IF EXISTS scd_operations;
-DROP TABLE IF EXISTS scd_cells_subscriptions;
 DROP TABLE IF EXISTS scd_subscriptions;
+DROP TYPE IF EXISTS operational_intent_state;
