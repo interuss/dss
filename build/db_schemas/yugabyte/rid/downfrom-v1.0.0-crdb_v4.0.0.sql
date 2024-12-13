842680f0f142fa787b7cd16b62e0d5d98e1585a6
Michael Barroco <michael@orbitalize.com>
2024-12-13 17:24:41 +0100

    [yugabyte] SQL Migrations (#1138)

---
+++ build/db_schemas/yugabyte/rid/downfrom-v1.0.0-crdb_v4.0.0.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS identification_service_areas;
+DROP TABLE IF EXISTS subscriptions;
+DROP TABLE IF EXISTS schema_versions;
--- cmds/db-manager/migration/migrate.go
+++ cmds/db-manager/migration/migrate.go
@@ -1,6 +1,7 @@
 package migration
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
@@ -73,26 +74,28 @@ func migrate(cmd *cobra.Command, _ []string) error {
 		}
 	}
 
-	// Connect to database server
-	connectParameters := crdbflags.ConnectParameters()
-	connectParameters.ApplicationName = "db-manager"
-	connectParameters.DBName = "postgres" // Use an initial database that is known to always be present
-	ds, err := datastore.Dial(ctx, connectParameters)
+	sysDbName := "postgres" // Use an initial database that is known to always be present
+	ds, err := connectTo(ctx, sysDbName)
 	if err != nil {
-		return fmt.Errorf("failed to connect to database with %+v: %w", connectParameters, err)
+		return fmt.Errorf("failed to connect to database %s: %w", sysDbName, err)
 	}
 	defer func() {
 		ds.Pool.Close()
 	}()
 
-	log.Printf("CRDB server version: %s", ds.Version.SemVer.String())
+	log.Printf("Datastore server type and version: %s@%s", ds.Version.Type, ds.Version.SemVer.String())
+
+	var (
+		isCockroach = ds.Version.Type == datastore.CockroachDB
+		isYugabyte  = ds.Version.Type == datastore.Yugabyte
+	)
 
 	// Make sure specified database exists
 	exists, err := ds.DatabaseExists(ctx, dbName)
 	if err != nil {
 		return fmt.Errorf("failed to check whether database %s exists: %w", dbName, err)
 	}
-	if !exists && dbName == "rid" {
+	if isCockroach && !exists && dbName == "rid" {
 		// In the special case of rid, the database was previously named defaultdb
 		log.Printf("Database %s does not exist; checking for older \"defaultdb\" database", dbName)
 		dbName = "defaultdb"
@@ -103,7 +106,7 @@ func migrate(cmd *cobra.Command, _ []string) error {
 	}
 	if !exists {
 		log.Printf("Database %s does not exist; creating now", dbName)
-		createDB := fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", dbName)
+		createDB := fmt.Sprintf("CREATE DATABASE %s", dbName)
 		if _, err := ds.Pool.Exec(ctx, createDB); err != nil {
 			return fmt.Errorf("failed to create new database %s: %v", dbName, err)
 		}
@@ -111,8 +114,17 @@ func migrate(cmd *cobra.Command, _ []string) error {
 		log.Printf("Database %s already exists; reading current state", dbName)
 	}
 
+	// Reconnect to target database
+	ds2, err := connectTo(ctx, dbName)
+	if err != nil {
+		return fmt.Errorf("failed to reconnect to database %s: %w", dbName, err)
+	}
+	defer func() {
+		ds2.Pool.Close()
+	}()
+
 	// Read current schema version of database
-	currentVersion, err := ds.GetSchemaVersion(ctx, dbName)
+	currentVersion, err := ds2.GetSchemaVersion(ctx, dbName)
 	if err != nil {
 		return fmt.Errorf("failed to get current database version for %s: %w", dbName, err)
 	}
@@ -155,29 +167,39 @@ func migrate(cmd *cobra.Command, _ []string) error {
 			return fmt.Errorf("failed to load SQL content from %s: %e", fullFilePath, err)
 		}
 
-		// Ensure SQL session has implicit transactions disabled for CRDB versions 22.2+
-		sessionConfigurationSQL := ""
-		if ds.Version.SemVer.Compare(*semver.New("22.2.0")) >= 0 {
-			sessionConfigurationSQL = "SET enable_implicit_transaction_for_batch_statements = false;\n"
+		migrationSQL := ""
+		if isCockroach {
+			// Ensure SQL session has implicit transactions disabled for CRDB versions 22.2+
+			sessionConfigurationSQL := ""
+			if ds2.Version.SemVer.Compare(*semver.New("22.2.0")) >= 0 {
+				sessionConfigurationSQL = "SET enable_implicit_transaction_for_batch_statements = false;\n"
+			}
+
+			migrationSQL = sessionConfigurationSQL + fmt.Sprintf("USE %s;\n", dbName) + string(rawMigrationSQL)
+		}
+		if isYugabyte {
+			// Migrations do not require database switch in opposite to CRDB.
+			migrationSQL = string(rawMigrationSQL)
 		}
 
-		migrationSQL := sessionConfigurationSQL + fmt.Sprintf("USE %s;\n", dbName) + string(rawMigrationSQL)
-
 		// Execute migration step
-		if _, err := ds.Pool.Exec(ctx, migrationSQL); err != nil {
+		if _, err := ds2.Pool.Exec(ctx, migrationSQL); err != nil {
 			return fmt.Errorf("failed to execute %s migration step %s: %w", dbName, fullFilePath, err)
 		}
 
 		// Update current state
-		if dbName == "defaultdb" && newVersion.String() == "4.0.0" && newCurrentStepIndex > currentStepIndex {
-			// RID database changes from `defaultdb` to `rid` when moving up to 4.0.0
-			dbName = "rid"
+		if isCockroach {
+			// Update current state for CRDB
+			if dbName == "defaultdb" && newVersion.String() == "4.0.0" && newCurrentStepIndex > currentStepIndex {
+				// RID database changes from `defaultdb` to `rid` when moving up to 4.0.0
+				dbName = "rid"
+			}
+			if dbName == "rid" && currentVersion.String() == "4.0.0" && newCurrentStepIndex < currentStepIndex {
+				// RID database changes from `rid` to `defaultdb` when moving down from 4.0.0
+				dbName = "defaultdb"
+			}
 		}
-		if dbName == "rid" && currentVersion.String() == "4.0.0" && newCurrentStepIndex < currentStepIndex {
-			// RID database changes from `rid` to `defaultdb` when moving down from 4.0.0
-			dbName = "defaultdb"
-		}
-		actualVersion, err := ds.GetSchemaVersion(ctx, dbName)
+		actualVersion, err := ds2.GetSchemaVersion(ctx, dbName)
 		if err != nil {
 			return fmt.Errorf("failed to get current database version for %s: %w", dbName, err)
 		}
@@ -190,6 +212,14 @@ func migrate(cmd *cobra.Command, _ []string) error {
 
 	log.Printf("Final %s version: %v", dbName, currentVersion)
 	return nil
+}
+
+func connectTo(ctx context.Context, dbName string) (*datastore.Datastore, error) {
+	// Connect to database server
+	connectParameters := crdbflags.ConnectParameters()
+	connectParameters.ApplicationName = "db-manager"
+	connectParameters.DBName = dbName
+	return datastore.Dial(ctx, connectParameters)
 }
 
 func enumerateMigrationSteps(path *string) ([]MigrationStep, error) {
