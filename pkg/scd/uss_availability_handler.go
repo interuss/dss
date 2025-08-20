a83ca56eaa489124bdca871ee7f9514ffa7d11c7
Michel Davit <michel.davit@orbitalize.com>
2025-08-20 15:18:00 +0200

    handle conflict

--- pkg/scd/uss_availability_handler.go
+++ pkg/scd/uss_availability_handler.go
@@ -62,9 +62,9 @@ func (a *Server) GetUssAvailability(ctx context.Context, req *restapi.GetUssAvai
 		if strings.Contains(err.Error(), "does not exist") {
 			response = GetDefaultAvailabilityResponse(id)
 		} else {
+			// No need to Propagate this error as this is not a useful stacktrace line
 			return restapi.GetUssAvailabilityResponseSet{Response500: &api.InternalServerErrorBody{
 				ErrorMessage: *dsserr.Handle(ctx, err)}}
-			// No need to Propagate this error as this is not a useful stacktrace line
 		}
 	}
 	return restapi.GetUssAvailabilityResponseSet{Response200: response}
@@ -94,7 +94,7 @@ func (a *Server) SetUssAvailability(ctx context.Context, req *restapi.SetUssAvai
 			Message: dsserr.Handle(ctx, stacktrace.PropagateWithCode(err, dsserr.BadRequest, "Invalid availability state"))}}
 	}
 	id := dssmodels.ManagerFromString(req.UssId)
-	oldVersion := scdmodels.OVN(req.Body.OldVersion)
+	version := scdmodels.OVN(req.Body.OldVersion)
 	ussareq := &scdmodels.UssAvailabilityStatus{
 		Uss:          id,
 		Availability: availability,
@@ -107,26 +107,23 @@ func (a *Server) SetUssAvailability(ctx context.Context, req *restapi.SetUssAvai
 			return stacktrace.Propagate(err, "Could not get USS availability from repo")
 		}
 		switch {
-		case old == nil && !oldVersion.Empty():
-			// no existing availability and update requested. Do not create and return default availability.
-			result = GetDefaultAvailabilityResponse(id)
-			return nil
-		case old != nil && old.Version != oldVersion:
-			// versions do not match. Do not update and return current USS availability.
-			result = &restapi.UssAvailabilityStatusResponse{
-				Status:  *old.ToRest(),
-				Version: old.Version.String(),
-			}
-			return nil
+		case old == nil && !version.Empty():
+			// The user wants set a new availability status but it already exists.
+			return stacktrace.NewErrorWithCode(dsserr.AlreadyExists, "availability for USS %s already exists", id.String())
+		case old != nil && old.Version != version:
+			// The user wants to update an availability status but the version doesn't match.
+			return stacktrace.Propagate(
+				stacktrace.NewErrorWithCode(dsserr.VersionMismatch, "USS availability version %s is not current", version),
+				"Current version is %s but client specified version %s", old.Version, version)
 		}
 
 		// Upsert the USS availability
 		ussa, err := r.UpsertUssAvailability(ctx, ussareq)
 		if err != nil {
-			return stacktrace.Propagate(err, "Could not upsert UssAvailability into repo")
+			return stacktrace.Propagate(err, "Could not upsert USS Availability into repo")
 		}
 		if ussa == nil {
-			return stacktrace.NewError("UssAvailability returned no Uss for ID: %s", req.UssId)
+			return stacktrace.NewError("UpsertUssAvailability returned no USS availability for ID: %s", id)
 		}
 		result = &restapi.UssAvailabilityStatusResponse{
 			Status:  *ussa.ToRest(),
@@ -136,13 +133,19 @@ func (a *Server) SetUssAvailability(ctx context.Context, req *restapi.SetUssAvai
 	}
 	err = a.Store.Transact(ctx, action)
 	if err != nil {
-		// In case of older DB versions where availability table doesn't exist
 		if strings.Contains(err.Error(), "does not exist") {
-			result = GetDefaultAvailabilityResponse(dssmodels.ManagerFromString(req.UssId))
+			// In case of older DB versions where availability table doesn't exist
+			result = GetDefaultAvailabilityResponse(id)
 		} else {
-			return restapi.SetUssAvailabilityResponseSet{Response500: &api.InternalServerErrorBody{
-				ErrorMessage: *dsserr.Handle(ctx, err)}}
-			// No need to Propagate this error as this is not a useful stacktrace line
+			err = stacktrace.Propagate(err, "Could not set USS availability status")
+			errResp := &restapi.ErrorResponse{Message: dsserr.Handle(ctx, err)}
+			switch stacktrace.GetCode(err) {
+			case dsserr.AlreadyExists, dsserr.VersionMismatch:
+				return restapi.SetUssAvailabilityResponseSet{Response409: errResp}
+			default:
+				return restapi.SetUssAvailabilityResponseSet{Response500: &api.InternalServerErrorBody{
+					ErrorMessage: *dsserr.Handle(ctx, stacktrace.Propagate(err, "Got an unexpected error"))}}
+			}
 		}
 	}
 
