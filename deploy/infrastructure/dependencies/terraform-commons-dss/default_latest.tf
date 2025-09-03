76dd0619d06b7f70624e7f8477ea792acd6affd2
Mariem Baccari <mariem.baccari@orbitalize.com>
2025-09-03 10:38:44 +0200

    add changes to terraform and helm chart files

--- deploy/infrastructure/dependencies/terraform-commons-dss/default_latest.tf
+++ deploy/infrastructure/dependencies/terraform-commons-dss/default_latest.tf
@@ -1,5 +1,8 @@
 locals {
-  rid_db_schema = var.desired_rid_db_version == "latest" ? (var.datastore_type == "cockroachdb" ? "4.0.0" : "1.0.1") : var.desired_rid_db_version
-  scd_db_schema = var.desired_scd_db_version == "latest" ? (var.datastore_type == "cockroachdb" ? "3.2.0" : "1.0.1") : var.desired_scd_db_version
+  rid_file_version = file("../../../../build/db_schemas/rid.version")
+  scd_file_version = file("../../../../build/db_schemas/scd.version")
+
+  rid_db_schema = var.desired_rid_db_version == "latest" ? (var.datastore_type == "cockroachdb" ? local.rid_file_version : "1.0.1") : var.desired_rid_db_version
+  scd_db_schema = var.desired_scd_db_version == "latest" ? (var.datastore_type == "cockroachdb" ? local.scd_file_version : "1.0.1") : var.desired_scd_db_version
   aux_db_schema = var.desired_aux_db_version == "latest" ? "1.0.0" : var.desired_aux_db_version
 }
