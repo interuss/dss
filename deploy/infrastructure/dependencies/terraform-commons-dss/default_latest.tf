472f16496906ed24fef48241bbcae83a2ba51e20
Mariem Baccari <mariem.baccari@orbitalize.com>
2025-09-04 10:44:45 +0200

    fix typo

--- deploy/infrastructure/dependencies/terraform-commons-dss/default_latest.tf
+++ deploy/infrastructure/dependencies/terraform-commons-dss/default_latest.tf
@@ -5,9 +5,7 @@ locals {
 
   rid_yugabyte_file_version = file("../../../../build/db_schemas/version/yugabyte/rid.version")
   scd_yugabyte_file_version = file("../../../../build/db_schemas/version/yugabyte/scd.version")
-
-}
-
+  
   rid_db_schema = var.desired_rid_db_version == "latest" ? (var.datastore_type == "cockroachdb" ? local.rid_crdb_file_version : local.rid_yugabyte_file_version) : var.desired_rid_db_version
   scd_db_schema = var.desired_scd_db_version == "latest" ? (var.datastore_type == "cockroachdb" ? local.scd_crdb_file_version : local.scd_yugabyte_file_version) : var.desired_scd_db_version
   aux_db_schema = var.desired_aux_db_version == "latest" ? local.aux_crdb_file_version : var.desired_aux_db_version
