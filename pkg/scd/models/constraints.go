73d320085bd142332169e73b559438daab4f64a3
Michel Davit <michel.davit@orbitalize.com>
2025-11-14 10:03:48 +0100

    [scd] return BadRequest when constraint time_end is before time_start

--- pkg/scd/models/constraints.go
+++ pkg/scd/models/constraints.go
@@ -5,9 +5,7 @@ import (
 
 	"github.com/golang/geo/s2"
 	restapi "github.com/interuss/dss/pkg/api/scdv1"
-	dsserr "github.com/interuss/dss/pkg/errors"
 	dssmodels "github.com/interuss/dss/pkg/models"
-	"github.com/interuss/stacktrace"
 )
 
 // Constraint models a constraint, as known by the DSS
@@ -52,23 +50,4 @@ func (c *Constraint) ToRest() *restapi.ConstraintReference {
 	}
 
 	return result
-}
-
-// ValidateTimeRange validates the time range of c.
-func (c *Constraint) ValidateTimeRange() error {
-	if c.StartTime == nil {
-		return stacktrace.NewErrorWithCode(dsserr.BadRequest, "Constraint must have an time_start")
-	}
-
-	// EndTime cannot be omitted for new Constraints.
-	if c.EndTime == nil {
-		return stacktrace.NewErrorWithCode(dsserr.BadRequest, "Constraint must have an time_end")
-	}
-
-	// EndTime cannot be before StartTime.
-	if c.EndTime.Sub(*c.StartTime) < 0 {
-		return stacktrace.NewErrorWithCode(dsserr.BadRequest, "Constraint time_end must be after time_start")
-	}
-
-	return nil
 }
