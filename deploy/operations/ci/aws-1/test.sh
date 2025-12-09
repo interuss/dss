c9d07cfd6976a635577a874ac91d6af971dc5995
Michael Barroco <michael@orbitalize.com>
2025-12-09 16:36:35 +0100

    Upgrade and improve tests reliability

--- deploy/operations/ci/aws-1/test.sh
+++ deploy/operations/ci/aws-1/test.sh
@@ -24,7 +24,6 @@ WORKSPACE_LOCATION="$(terraform output -raw workspace_location)"
 # Login into the Kubernetes Cluster
 cd "${WORKSPACE_LOCATION}"
 ./get-credentials.sh
-aws sts get-caller-identity
 
 # Allow access to the cluster to AWS admins
 kubectl apply -f "aws_auth_config_map.yml"
@@ -37,16 +36,20 @@ kubectl apply -f "aws_auth_config_map.yml"
 cd "$BASEDIR/../../../services/helm-charts/dss"
 RELEASE_NAME="dss"
 helm dep update --kube-context="$KUBE_CONTEXT"
-helm upgrade --install --debug --kube-context="$KUBE_CONTEXT" -f "${WORKSPACE_LOCATION}/helm_values.yml" "$RELEASE_NAME" .
+helm upgrade --install --kube-context="$KUBE_CONTEXT" -f "${WORKSPACE_LOCATION}/helm_values.yml" "$RELEASE_NAME" .
 kubectl wait --for=condition=complete --timeout=3m job --all
 
+# Write infrastructure information
+
+
 # Test the deployment of the DSS
-kubectl apply -f "$BASEDIR/test-resources.yaml"
+kubectl apply --force -f "$BASEDIR/test-resources.yaml"
 kubectl create secret generic -n tests dummy-oauth-certs --from-file="$BASEDIR/../../../../build/test-certs/auth2.key"
-kubectl wait -n tests --for=condition=complete --timeout=10m job.batch/uss-qualifier
+kubectl wait -n tests --for=condition=complete --for=condition=failed --timeout=10m job.batch/uss-qualifier
+# TODO: Verify outcome of the job
+
 # dummy-oauth-certs secret is deleted with the namespace using the command below
 kubectl delete -f "$BASEDIR/test-resources.yaml"
-
 
 if [ -n "$DO_NOT_DESTROY" ]; then
   echo "Destroy disabled. Exit."
