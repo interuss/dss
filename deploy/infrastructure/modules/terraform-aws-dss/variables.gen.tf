9777a8b6885a3a5bfcd3a99e8f24858963c4e106
Maximilien Cuony <the-glu@users.noreply.github.com>
2026-01-14 16:23:58 +0100

    [terraform] Change Kubernetes version supported from 1.24-1.32 to 1.28-1.34 (#1343)

--- deploy/infrastructure/modules/terraform-aws-dss/variables.gen.tf
+++ deploy/infrastructure/modules/terraform-aws-dss/variables.gen.tf
@@ -112,12 +112,12 @@ variable "kubernetes_version" {
   description = <<-EOT
   Desired version of the Kubernetes cluster control plane and nodes.
 
-  Supported versions: 1.24 to 1.32
+  Supported versions: 1.28 to 1.34
   EOT
 
   validation {
-    condition     = contains(["1.24", "1.25", "1.26", "1.27", "1.28", "1.29", "1.30", "1.31", "1.32"], var.kubernetes_version)
-    error_message = "Supported versions: 1.24 to 1.32"
+    condition     = contains(["1.28", "1.29", "1.30", "1.31", "1.32", "1.33", "1.34"], var.kubernetes_version)
+    error_message = "Supported versions: 1.28 to 1.34"
   }
 }
 
