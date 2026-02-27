087dab5783e90894f4b153659afdfb32244a01f6
Maximilien Cuony <the-glu@users.noreply.github.com>
2026-02-27 19:41:11 +0100

    [yugabyte] Fix migrations (#1383)

--- build/db_schemas/yugabyte/scd/downfrom-v1.1.0-remove_locks.sql
+++ build/db_schemas/yugabyte/scd/downfrom-v1.1.0-remove_locks.sql
@@ -1,3 +1,3 @@
 DROP TABLE IF EXISTS scd_locks;
 
-UPDATE schema_versions set schema_version = 'v1.0.0' WHERE onerow_enforcer = TRUE;
+UPDATE schema_versions set schema_version = 'v1.0.1' WHERE onerow_enforcer = TRUE;
