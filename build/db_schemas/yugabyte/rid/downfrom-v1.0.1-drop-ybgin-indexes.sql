cfc5ecc978e73327be7f3eaad16c6069ae5e7389
Maximilien Cuony <the-glu@users.noreply.github.com>
2026-02-27 17:13:33 +0100

    [tooling] Add automatic database migration tests (#1376)

--- build/db_schemas/yugabyte/rid/downfrom-v1.0.1-drop-ybgin-indexes.sql
+++ build/db_schemas/yugabyte/rid/downfrom-v1.0.1-drop-ybgin-indexes.sql
@@ -1,2 +1,4 @@
 CREATE INDEX s_cell_idx ON subscriptions USING ybgin (cells);
 CREATE INDEX isa_cell_idx ON identification_service_areas USING ybgin (cells);
+
+UPDATE schema_versions set schema_version = 'v1.0.0' WHERE onerow_enforcer = TRUE;
--- build/db_schemas/yugabyte/scd/downfrom-v1.0.1-drop-ybgin-indexes.sql
+++ build/db_schemas/yugabyte/scd/downfrom-v1.0.1-drop-ybgin-indexes.sql
@@ -1,3 +1,5 @@
 CREATE INDEX sc_cells_idx ON scd_constraints USING ybgin (cells);
 CREATE INDEX ss_cells_idx ON scd_subscriptions USING ybgin (cells);
 CREATE INDEX so_cells_idx ON scd_operations USING ybgin (cells);
+
+UPDATE schema_versions set schema_version = 'v1.0.0' WHERE onerow_enforcer = TRUE;
