e3f68024e8e83beb1907d30e44d651f8591fc5ba
Maximilien Cuony <maximilien.cuony@orbitalize.com>
2026-04-22 14:43:19 +0200

    [store] Add store type flag

--- pkg/scd/store/store.go
+++ pkg/scd/store/store.go
@@ -6,6 +6,8 @@ import (
 	"github.com/interuss/dss/pkg/scd/repos"
 	scdsqlstore "github.com/interuss/dss/pkg/scd/store/sqlstore"
 	dssstore "github.com/interuss/dss/pkg/store"
+	"github.com/interuss/dss/pkg/store/params"
+	"github.com/interuss/stacktrace"
 	"go.uber.org/zap"
 )
 
@@ -15,5 +17,11 @@ type Store = dssstore.Store[repos.Repository]
 
 // Init selects and initializes the scd store backend.
 func Init(ctx context.Context, logger *zap.Logger, withCheckCron bool, globalLock bool) (Store, error) {
-	return scdsqlstore.Init(ctx, logger, withCheckCron, globalLock)
+	storeType := params.GetStoreParameters().StoreType
+	switch storeType {
+	case "sql":
+		return scdsqlstore.Init(ctx, logger, withCheckCron, globalLock)
+	default:
+		return nil, stacktrace.NewError("Unsupported store type %q for scd", storeType)
+	}
 }
