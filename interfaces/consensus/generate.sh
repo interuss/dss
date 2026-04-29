04c6c7cd180db848cd018498f87aa8b9cbf18609
Mariem Baccari <mariem.baccari@orbitalize.com>
2026-04-29 16:47:50 +0200

    move proto under grpc folder

--- interfaces/consensus/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-cd "$(dirname "$0")"
-
-protoc --go_out=../../pkg/grpc --go_opt=paths=source_relative \
-       --go-grpc_out=../../pkg/grpc --go-grpc_opt=paths=source_relative \
-       consensus.proto
-
