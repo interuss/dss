6e86c0cadbe4f5ebb8be773d8b750c885cf48187
Mariem Baccari <mariem.baccari@orbitalize.com>
2026-05-04 18:58:56 +0200

    add raftstore dir

---
+++ pkg/raftstore/store.go
@@ -0,0 +1,26 @@
+package raftstore
+
+import (
+	"context"
+)
+
+type Store[R any] struct{}
+
+func Init[R any]() *Store[R] {
+	return &Store[R]{}
+}
+
+// Transact proposes the entry to Raft and blocks until it is committed and applied.
+func (s *Store[R]) Transact(ctx context.Context, f func(context.Context, R) error) error {
+	panic("not implemented")
+}
+
+// Interact returns a read-only repository that can be used to query the store without proposing a Raft entry.
+func (s *Store[R]) Interact(_ context.Context) (R, error) {
+	panic("not implemented")
+}
+
+// Close shuts down the consensus instance.
+func (s *Store[R]) Close() error {
+	panic("not implemented")
+}
