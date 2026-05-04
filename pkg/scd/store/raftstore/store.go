95c46f5c92e1a9c8b3ce0d40a426568b6e8f9786
Mariem Baccari <mariem.baccari@orbitalize.com>
2026-05-04 17:03:07 +0200

    move to pkg/raftstore

--- pkg/scd/store/raftstore/store.go
+++ pkg/scd/store/raftstore/store.go
@@ -3,19 +3,11 @@ package raftstore
 import (
 	"github.com/interuss/dss/pkg/raftstore"
 	"github.com/interuss/dss/pkg/scd/repos"
-	"go.uber.org/zap"
 )
 
 // repo is a full implementation of scd.repos.Repository for Raft-based in-memory storage.
-type repo struct {
-	store *Store
-}
+type repo struct{}
 
-func Init(logger *zap.Logger) (*raftstore.Store[repos.Repository], error) {
-	return raftstore.Init
-}
-
-// Store implements store.Store[repos.Repository] for Raft-based in-memory storage.
-type Store struct {
-	logger *zap.Logger
+func Init() (*raftstore.Store[repos.Repository], error) {
+	return raftstore.Init[repos.Repository](), nil
 }
