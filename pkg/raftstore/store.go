063810c278e68157348ca29847b616f88c0a208f
Mariem Baccari <mariem.baccari@orbitalize.com>
2026-05-05 13:49:12 +0200

    remove panics from store.go

--- pkg/raftstore/store.go
+++ pkg/raftstore/store.go
@@ -2,6 +2,9 @@ package raftstore
 
 import (
 	"context"
+
+	dsserr "github.com/interuss/dss/pkg/errors"
+	"github.com/interuss/stacktrace"
 )
 
 type Store[R any] struct{}
@@ -12,15 +15,16 @@ func Init[R any]() *Store[R] {
 
 // Transact proposes the entry to Raft and blocks until it is committed and applied.
 func (s *Store[R]) Transact(ctx context.Context, f func(context.Context, R) error) error {
-	panic("not implemented")
+	return stacktrace.NewErrorWithCode(dsserr.NotImplemented, "Transact not yet implemented for Raft store")
 }
 
 // Interact returns a repository that can be used to query the store without proposing a Raft entry.
 func (s *Store[R]) Interact(_ context.Context) (R, error) {
-	panic("not implemented")
+	var empty R
+	return empty, stacktrace.NewErrorWithCode(dsserr.NotImplemented, "Interact not yet implemented for Raft store")
 }
 
 // Close shuts down the consensus instance.
 func (s *Store[R]) Close() error {
-	panic("not implemented")
+	return stacktrace.NewErrorWithCode(dsserr.NotImplemented, "Close not yet implemented for Raft store")
 }
