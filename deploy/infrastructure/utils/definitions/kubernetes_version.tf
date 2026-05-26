93a2e22df3c018ed27909fe3a8c7148c61970905
Maximilien Cuony <maximilien.cuony@orbitalize.com>
2026-05-26 09:44:11 +0200

    [terraform] Upgrade to kubernetess version 1.35

--- deploy/infrastructure/utils/definitions/kubernetes_version.tf
+++ deploy/infrastructure/utils/definitions/kubernetes_version.tf
@@ -3,11 +3,11 @@ variable "kubernetes_version" {
   description = <<-EOT
   Desired version of the Kubernetes cluster control plane and nodes.
 
-  Supported versions: 1.28 to 1.34
+  Supported versions: 1.28 to 1.35
   EOT
 
   validation {
-    condition     = contains(["1.28", "1.29", "1.30", "1.31", "1.32", "1.33", "1.34"], var.kubernetes_version)
-    error_message = "Supported versions: 1.28 to 1.34"
+    condition     = contains(["1.28", "1.29", "1.30", "1.31", "1.32", "1.33", "1.34", "1.35"], var.kubernetes_version)
+    error_message = "Supported versions: 1.28 to 1.35"
   }
 }
