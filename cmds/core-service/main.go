bd136d81ce721e353df81600c5763af6f31f4f8d
Maximilien Cuony <maximilien.cuony@orbitalize.com>
2026-06-09 11:30:49 +0200

    [core] Fix timeout argument

--- cmds/core-service/main.go
+++ cmds/core-service/main.go
@@ -46,7 +46,8 @@ var (
 	enableSCD         = flag.Bool("enable_scd", false, "Enables the Strategic Conflict Detection API")
 	allowHTTPBaseUrls = flag.Bool("allow_http_base_urls", false, "Enables http scheme for Strategic Conflict Detection API")
 	enableHTTP        = flag.Bool("enable_http", false, "DEPRECATED (replaced by allow_http_base_urls): Enables http scheme for Strategic Conflict Detection API")
-	timeout           = flag.Duration("server timeout", 10*time.Second, "Default timeout for server calls")
+	legacyTimeout     = flag.Duration("server timeout", 0, "Default timeout for server calls (legacy)")
+	timeout           = flag.Duration("server_timeout", 10*time.Second, "Default timeout for server calls")
 	locality          = flag.String("locality", "", "self-identification string of this DSS instance")
 	publicEndpoint    = flag.String("public_endpoint", "", "Public endpoint to access this DSS instance. Must be an absolute URI")
 
@@ -330,6 +331,12 @@ func RunHTTPServer(ctx context.Context, ctxCanceler func(), address, locality st
 
 		scdV1Router := apiscdv1.MakeAPIRouter(scdV1Server, authorizer)
 		multiRouter.Routers = append(multiRouter.Routers, &scdV1Router)
+	}
+
+	if *legacyTimeout != 0 {
+		*timeout = *legacyTimeout
+
+		logger.Warn("'server timeout' has been renamed to 'server_timeout'")
 	}
 
 	// the middlewares are wrapped and, therefore, executed in the opposite order
