7eb7876d102823bcab9a2fb828cbc367c6a66268
Mariem Baccari <53703829+MariemBaccari@users.noreply.github.com>
2026-08-19 16:22:12 +0200

    [raft/rid] Extract delete subscription (#1597)

--- pkg/rid/server/v2/server.go
+++ pkg/rid/server/v2/server.go
@@ -4,10 +4,12 @@ import (
 	"github.com/robfig/cron/v3"
 
 	"github.com/interuss/dss/pkg/rid/application"
+	"github.com/interuss/dss/pkg/rid/store"
 )
 
 // Server implements ridv2.Implementation.
 type Server struct {
+	Store             store.Store
 	App               application.App
 	Locality          string
 	AllowHTTPBaseUrls bool
