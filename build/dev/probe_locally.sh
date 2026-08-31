257b312ea5dab04096f9f6d14fd4b50d84b7d03e
Mariem Baccari <mariem.baccari@orbitalize.com>
2026-08-31 14:16:56 +0200

    decrease to 1

--- build/dev/probe_locally.sh
+++ build/dev/probe_locally.sh
@@ -61,9 +61,9 @@ fi
 
 # Following the extension of performance testing in https://github.com/interuss/dss/pull/1618
 # The Yugabyte test with the -enable_scd_hash_lock flag faces intermittent load-sensitive test failures, so we reduce the
-# concurrent worker count to 2 for that specific configuration.
+# concurrent worker count to 1 for that specific configuration.
 if [[ "${COMPOSE_PROFILES:-}" == *"yugabyte"* && "${CORE_SERVICE_EXTRA_FLAGS:-}" == *"-enable_scd_hash_lock"* ]]; then
-	PROBER_EXTRA_ARGS+=(--heavy_traffic_concurrent_workers 2)
+	PROBER_EXTRA_ARGS+=(--heavy_traffic_concurrent_workers 1)
 fi
 
 if ! docker run --rm --link "$OAUTH_CONTAINER":oauth \
